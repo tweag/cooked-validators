d255fd3ac29ae198365198a2ec60abe1fd4ec9f9
Victor Miraldo <victor.miraldo@tweag.io>
2021-10-15 18:02:36 +0200

    Add Empty trace

--- cooked-validators/src/Cooked/Traces.hs
+++ cooked-validators/src/Cooked/Traces.hs
@@ -15,14 +15,17 @@ modifyTxSkelGen :: (Monad m)
 modifyTxSkelGen f g = fmap (first f) . g
 
 data Tr m a b where
+  Empty   :: Tr m a a
   Step    :: TxSkelGen m a b -> Tr m b c -> Tr m a c
   Stutter :: Tr m a b -> Tr m a b
 
 modifyTr :: (Monad m) => (TxSkel -> TxSkel) -> Tr m a b -> Tr m a b
+modifyTr f Empty        = Empty
 modifyTr f (Step g tr)  = Step (modifyTxSkelGen f g) (modifyTr f tr)
 modifyTr f (Stutter tr) = Stutter (modifyTr f tr)
 
 executeTr :: (Monad m) => Tr m a b -> a -> MockChainT m b
+executeTr Empty        a = return a
 executeTr (Stutter tr) a = executeTr tr a
 executeTr (Step g tr)  a = do
   (txSkel, b) <- g a
