31b4882a5ad22f9fcd944c5478a8589e6d5f5ab8
Guillaume Genestier <guillaume.genestier@tweag.io>
2021-10-18 11:37:22 +0200

    Cleaning of Balance to address #2

--- cooked-validators/src/Cooked/Balance.hs
+++ cooked-validators/src/Cooked/Balance.hs
@@ -1,57 +1,63 @@
 module Cooked.Balance where
 
 import Data.Default
-import Control.Arrow (second, (***))
-import Control.Monad.Identity
+import Control.Arrow ((***))
 import qualified Data.Set as S
-import Ledger.Contexts
-import Ledger.Index
-import Ledger.Value
-import Ledger.Constraints
-import Ledger.Constraints.OffChain hiding (tx)
-import Ledger.Tx
-import Ledger.Orphans                   ()
-import Ledger.TimeSlot (posixTimeRangeToContainedSlotRange)
 
-import Plutus.Contract.Trace
-import Plutus.V1.Ledger.Address
-import Plutus.V1.Ledger.Credential      (Credential (..))
-import qualified PlutusTx.Numeric as Num
+import qualified Ledger.Contexts as Pl
+import qualified Ledger.Index as Pl
+import qualified Ledger.Value as Pl
+import qualified Ledger.Constraints as Pl
+import qualified Ledger.Constraints.OffChain as Pl
+import qualified Ledger.Tx as Pl
+import qualified Ledger.TimeSlot as Pl
+
+import qualified Plutus.Contract.Trace as Pl
+import qualified Plutus.V1.Ledger.Address as Pl
+import qualified Plutus.V1.Ledger.Crypto as Pl
+import qualified Plutus.V1.Ledger.Credential as Pl
+import qualified PlutusTx.Numeric as Pl
 
 import Cooked.MockChain
 
 -- |Balances a transaction with money from a given wallet. For every transaction,
 -- it must be the case that @inputs + mint == outputs + fee@.
-balanceTxFrom :: (Monad m) => Wallet -> UnbalancedTx -> MockChainT m Tx
-balanceTxFrom w (UnbalancedTx tx0 _reqSigs _uindex slotRange) = do
+balanceTxFrom :: (Monad m) => Pl.Wallet -> Pl.UnbalancedTx -> MockChainT m Pl.Tx
+balanceTxFrom w (Pl.UnbalancedTx tx0 _reqSigs _uindex slotRange) = do
   -- We start by gathering all the inputs and summing it
-  let tx = tx0 { txFee = minFee tx0 }
-  lhsInputs <- mapM (outsFromRef . txInRef) (S.toList (txInputs tx))
-  let lhs = mappend (mconcat $ map txOutValue lhsInputs)      (txMint tx)
-  let rhs = mappend (mconcat $ map txOutValue $ txOutputs tx) (txFee tx)
-  -- We then go through the output value and investigate for each token
-  -- if there is a way to balance it.
-  (leftover, usedUTxO) <-
-    foldM (\(leftOver, usedUTxO) (a,t,_) -> addInputToEquilibrate lhs rhs a t leftOver usedUTxO)
-      (mempty, [])
-      (flattenValue rhs)
+  let tx = tx0 { Pl.txFee = Pl.minFee tx0 }
+  lhsInputs <- mapM (outsFromRef . Pl.txInRef) (S.toList (Pl.txInputs tx))
+  let lhs = mappend (mconcat $ map Pl.txOutValue lhsInputs)      (Pl.txMint tx)
+  let rhs = mappend (mconcat $ map Pl.txOutValue $ Pl.txOutputs tx) (Pl.txFee tx)
+  let wPKH = Pl.pubKeyHash (Pl.walletPubKey w)
+  (usedUTxOs, leftOver) <- balanceWithUTxOsOf (rhs Pl.- lhs) wPKH
   -- All the UTxOs signed by the sender of the transaction and useful to balance it
   -- are added to the inputs.
-  let txIns' = map (`TxIn` Just ConsumePublicKeyAddress) usedUTxO
+  let txIns' = map (`Pl.TxIn` Just Pl.ConsumePublicKeyAddress) usedUTxOs
   -- A new output is opened with the leftover of the added inputs.
-  let txOut' =
-        TxOut
-          (Address (PubKeyCredential $ pubKeyHash $ walletPubKey w) Nothing)
-          leftover Nothing
-  return tx{ txInputs  = txInputs tx <> S.fromList txIns'
-           , txOutputs = txOutputs tx ++ [txOut']
-           , txValidRange = posixTimeRangeToContainedSlotRange def slotRange
+  let txOut' = Pl.TxOut (Pl.Address (Pl.PubKeyCredential wPKH) Nothing) leftOver Nothing
+  return tx{ Pl.txInputs  = Pl.txInputs tx <> S.fromList txIns'
+           , Pl.txOutputs = Pl.txOutputs tx ++ [txOut']
+           , Pl.txValidRange = Pl.posixTimeRangeToContainedSlotRange def slotRange
            }
+
+balanceWithUTxOsOf :: (Monad m)
+                   => Pl.Value -> Pl.PubKeyHash -> MockChainT m ([Pl.TxOutRef], Pl.Value)
+balanceWithUTxOsOf val wPKH =
+  spendValueFrom val <$> utxosFromPK' wPKH
+
+spendValueFrom :: Pl.Value -> [(Pl.TxOutRef, Pl.TxOut)] -> ([Pl.TxOutRef], Pl.Value)
+spendValueFrom val utxos =
+  -- We then go through the output value and investigate for each token
+  -- if there is a way to balance it.
+  foldl (\(leftO, spentTx) (curr,tok,i) -> addInputToBalance curr tok i leftO spentTx)
+    ([], mempty)
+    (Pl.flattenValue val)
   where
-    addInputToEquilibrate lhs rhs asset token leftOver usedUTxO =
-      let delta = valueOf rhs asset token - valueOf lhs asset token in
-      case compare delta 0 of
-      EQ -> return (leftOver, usedUTxO)
+    addInputToBalance :: Pl.CurrencySymbol -> Pl.TokenName -> Integer
+                      -> [Pl.TxOutRef] -> Pl.Value -> ([Pl.TxOutRef], Pl.Value)
+    addInputToBalance curr token i usedUTxO leftOver =
+      if i < 0
       -- If the input of the transaction is already too big,
       -- then no matter which inputs we add, the transaction will fail.
       -- We still send it, in order to get the error of the chain.
@@ -59,26 +65,26 @@ balanceTxFrom w (UnbalancedTx tx0 _reqSigs _uindex slotRange) = do
       -- We could have try to balance automatically more transaction
       -- by simply sending the leftover to the creator of the transaction.
       -- However, in most of the case, allocating the whole money exchanged in the transaction
-      GT -> return (leftOver, usedUTxO)
-      LT ->
-        let thisTokLeftOver = valueOf leftOver asset token in
-        if delta > thisTokLeftOver
-        then do
+      -- is the expected situation, hence we want the author to be aware of this oversight.
+      then (usedUTxO, leftOver)
+      else
+        let thisTokLeftOver = Pl.valueOf leftOver curr token in
+        if i > thisTokLeftOver
+        then
           -- Even while using the leftover, we still are looking for some money.
-          -- Hence, we go through all the outputs of the transaction creator.
-          refsFromW <- utxosFromPK' (pubKeyHash $ walletPubKey w)
-          (neededRefsFromW, additionalLeftover) <-
-            necessaryUtxosFor asset token (delta - thisTokLeftOver) refsFromW usedUTxO
+          let (newUsedUTxOs, additionalLeftover) =
+                necessaryUtxosFor curr token (i - thisTokLeftOver) utxos usedUTxO
+          in
           -- The part taken form the leftover, is not in the previous leftover anymore.
-          let prevLeftOver = leftOver Num.- singleton asset token thisTokLeftOver
-          return (prevLeftOver <> additionalLeftover, usedUTxO ++ neededRefsFromW)
+          let prevLeftOver = leftOver Pl.- Pl.singleton curr token thisTokLeftOver in
+          (usedUTxO ++ newUsedUTxOs, prevLeftOver <> additionalLeftover)
         -- If the leftover contains more (or exactly) money than required,
         -- then it is simply taken from the left-over.
-        else return (leftOver Num.- singleton asset token delta, usedUTxO)
+        else (usedUTxO, leftOver Pl.- Pl.singleton curr token i)
 
--- Given an the asset name (both currency and token),
--- an integer n, a list of UTxOs and the subset of those, which are already used,
--- return the TxOutRes we need to consume
+-- Given an asset name (both currency and token),
+-- an integer n, a list of UTxOs and the subset of those which are already used,
+-- return the TxOutRef we need to consume
 -- in order to cover n and returns any potential leftover that need to be
 -- transfered to n's owner again.
 --
@@ -87,33 +93,30 @@ balanceTxFrom w (UnbalancedTx tx0 _reqSigs _uindex slotRange) = do
 -- > necessaryUtxosFor a t 100 [(o1, 50, dh1), (o2, 30, dh2), (o3, 30, dh3), (o4, 120, dh4)] []
 -- >    == ([o1, o2, o3], 10)
 --
-necessaryUtxosFor :: (Monad m)
-                  => CurrencySymbol -> TokenName
-                  -> Integer -> [(TxOutRef, TxOut)]
-                  -> [TxOutRef]
-                  -> MockChainT m ([TxOutRef], Value)
-necessaryUtxosFor _     _     0 _  _ = return ([], mempty)
-necessaryUtxosFor asset token n ((oref, o):os) usedUTxO
+necessaryUtxosFor :: Pl.CurrencySymbol -> Pl.TokenName -> Integer
+                  -> [(Pl.TxOutRef, Pl.TxOut)] -> [Pl.TxOutRef] -> ([Pl.TxOutRef], Pl.Value)
+necessaryUtxosFor _     _     0 _  _ = ([], mempty)
+necessaryUtxosFor curr token n ((oref, o):os) usedUTxO
   -- If an output has already been added to the transaction previously,
   -- it is considered in the leftover, which had been taken into account earlier in the process.
-  | oref `elem` usedUTxO = necessaryUtxosFor asset token n os usedUTxO
+  | oref `elem` usedUTxO = necessaryUtxosFor curr token n os usedUTxO
   -- If the output has more than required, we add it to the transaction
   -- and update the leftover accordingly.
-  | valOf (txOutValue o) > n = return ([oref], txOutValue o Num.- singleton asset token n)
+  | valOf (Pl.txOutValue o) > n = ([oref], Pl.txOutValue o Pl.- Pl.singleton curr token n)
   -- If the output does not contain the token we are looking for at all,
   -- then it is pointless to add it in the transaction.
-  | valOf (txOutValue o) == 0 = necessaryUtxosFor asset token n os usedUTxO
+  | valOf (Pl.txOutValue o) == 0 = necessaryUtxosFor curr token n os usedUTxO
   -- If the output partially fulfill our expectation, we grab it into the transaction,
   -- and continue our investigation.
   | otherwise =
-      let foundAmount = valOf (txOutValue o) in
-      let valLeftover = txOutValue o Num.- singleton asset token foundAmount in
-      ((oref :) *** (valLeftover Num.+ ))
-        <$> necessaryUtxosFor asset token (n - foundAmount) os usedUTxO
+      let foundAmount = valOf (Pl.txOutValue o) in
+      let valLeftover = Pl.txOutValue o Pl.- Pl.singleton curr token foundAmount in
+      ((oref :) *** (valLeftover Pl.+ )) $
+        necessaryUtxosFor curr token (n - foundAmount) os usedUTxO
 
-  where valOf val = valueOf val asset token
+  where valOf val = Pl.valueOf val curr token
 -- If there is not enough funds, the transaction will fail.
 -- Even if we are already aware of it at this point,
 -- we do not want to interfere with the error generated by the contract,
 -- hence we let the wrong transaction happen.
-necessaryUtxosFor _     _     _ [] _ = return ([], mempty)
+necessaryUtxosFor _     _     _ [] _ = ([], mempty)
