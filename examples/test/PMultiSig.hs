0f196517b502944980c5634d236392d368441e90
Victor Miraldo <victor.miraldo@tweag.io>
2021-10-19 16:13:02 +0200

    Add a fully worked example and fix the PMultiSig contract

--- examples/test/PMultiSig.hs
+++ examples/test/PMultiSig.hs
@@ -2,9 +2,10 @@ module Main where
 
 import Data.Default
 
-import qualified Ledger          as Pl
-import qualified Ledger.TimeSlot as Pl
-import qualified Ledger.Ada      as Pl
+import qualified Ledger           as Pl
+import qualified Ledger.TimeSlot  as Pl
+import qualified Ledger.Ada       as Pl
+import qualified PlutusTx.Prelude as Pl
 
 import Cooked.MockChain
 import Cooked.Traces
@@ -13,18 +14,43 @@ import Cooked.Tx.Generator
 
 import PMultiSig
 
-txCreatePayment :: Wallet -> Payment -> TxSkelGen Params Params
-txCreatePayment w pmt params = return (TxSkel w ctrs , params)
+params :: Params
+params = Params (map walletPK knownWallets) 2
+
+txCreatePayment :: Wallet -> Payment -> MockChain TxSkel
+txCreatePayment w pmt = return (TxSkel w ctrs)
   where
-    ctrs = [PaysScript (pmultisig params) [(Proposal pmt, paymentAmount pmt)]]
+    ctrs = [PaysScript (pmultisig params) [(Proposal pmt, Pl.lovelaceValueOf (paymentAmount pmt))]]
 
+txSign :: Wallet -> Payment -> MockChain TxSkel
+txSign w pmt =
+  let wSignature = Pl.sign (Pl.sha2_256 $ packPayment pmt) (snd w)
+      wPk        = walletPK w
+      ctrs       = [PaysScript (pmultisig params) [(Sign wPk wSignature, mempty)]]
+   in return (TxSkel w ctrs)
 
+isProposal :: Datum -> a -> Bool
+isProposal (Proposal _) _ = True
+isProposal _            _ = False
 
+isSignature :: Datum -> a -> Bool
+isSignature (Sign _ _) _ = True
+isSignature _          _ = False
 
-params      = Params (map walletPK knownWallets) 2
-samplePmt s = Payment (Pl.lovelaceValueOf 10) (walletPKHash $ wallet 3) (Pl.slotToBeginPOSIXTime def s)
+txExecute :: Wallet -> MockChain TxSkel
+txExecute w = do
+  [(propOut, Proposal prop)] <- scriptUtxosSuchThat (pmultisig params) isProposal
+  sigs              <- scriptUtxosSuchThat (pmultisig params) isSignature
+  let txConstr = PaysPK (paymentRecipient prop) (Pl.lovelaceValueOf $ paymentAmount prop)
+               : SpendsScript (pmultisig params) () (propOut, Proposal prop)
+               : map (SpendsScript (pmultisig params) ()) sigs
+  return (TxSkel w txConstr)
+
+samplePmt = Payment 4200 (walletPKHash $ wallet 3)
 
 run1 = runMockChain $ do
-  s <- slot
-  (skel, _) <- txCreatePayment (wallet 1) (samplePmt s) params
-  validateTxFromSkeleton skel
+  txCreatePayment (wallet 1) samplePmt >>= validateTxFromSkeleton
+  txSign          (wallet 1) samplePmt >>= validateTxFromSkeleton
+  txSign          (wallet 2) samplePmt >>= validateTxFromSkeleton
+  txSign          (wallet 3) samplePmt >>= validateTxFromSkeleton
+  txExecute       (wallet 4) >>= validateTxFromSkeleton
