defaaefe618abc97d2fde046c0660b9572cd1ba9
Florent C <florent.chevrou@tweag.io>
2021-10-25 11:21:45 +0200

    Use `prettyprinter` lib in UtxoStatePrinter

--- cooked-validators/src/Cooked/MockChain/UtxoStatePrinter.hs
+++ cooked-validators/src/Cooked/MockChain/UtxoStatePrinter.hs
@@ -1,86 +1,101 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Cooked.MockChain.UtxoStatePrinter (showUtxoState) where
+module Cooked.MockChain.UtxoStatePrinter (prettyUtxoState) where
 
 import Cooked.MockChain.Base (UtxoState)
-import Data.List (intercalate)
 import qualified Data.Map as Map (toList)
 import Data.Proxy (Proxy)
 import qualified Ledger as Pl
 import qualified Ledger.Value as Pl
 import qualified Plutus.V2.Ledger.Api as Pl
-import qualified PlutusPrelude as Pl (pretty)
 import qualified PlutusTx.AssocMap as Pl
+import Prettyprinter (Doc)
+import qualified Prettyprinter
 
-showTokenValue :: (Pl.CurrencySymbol, Pl.Map Pl.TokenName Integer) -> String
-showTokenValue (symb, amountMap) =
+prettyTokenValue :: (Pl.CurrencySymbol, Pl.Map Pl.TokenName Integer) -> Doc ann
+prettyTokenValue (symb, amountMap) =
   case (symb, Pl.toList amountMap) of
-    ("", [("", adaAmount)]) -> "Ada: " <> show (Pl.pretty adaAmount)
+    ("", [("", adaAmount)]) ->
+      "Ada" <> Prettyprinter.colon <> Prettyprinter.pretty adaAmount
     (_, tokenValueMap) ->
-      show (Pl.pretty symb) <> ": " <> show (Pl.pretty tokenValueMap)
+      Prettyprinter.pretty symb
+        <> Prettyprinter.colon
+        <> Prettyprinter.pretty tokenValueMap
 
 -- Unsafe: address carries either pubkey or validator hash but
 -- the API does not expose the constructors to pattern match.
-showAddressTypeAndHash :: Pl.Address -> String
-showAddressTypeAndHash a =
+prettyAddressTypeAndHash :: Pl.Address -> Doc ann
+prettyAddressTypeAndHash a =
   case Pl.toPubKeyHash a of
     Nothing ->
       case Pl.toValidatorHash a of
         Nothing -> error "Printing address: Neither pubkey nor validator hash"
-        Just hash -> "script " <> show (Pl.pretty hash)
-    Just hash -> "pubkey " <> show (Pl.pretty hash)
+        Just hash ->
+          "script" <> Prettyprinter.colon <> Prettyprinter.pretty hash
+    Just hash ->
+      "pubkey" <> Prettyprinter.colon <> Prettyprinter.pretty hash
 
-showValue :: Pl.Value -> String
-showValue =
-  (\s -> if null s then "" else "{ " <> s <> " }\n")
-    . intercalate "; "
-    . map showTokenValue
+prettyValue :: Pl.Value -> Doc ann
+prettyValue =
+  Prettyprinter.encloseSep "{" "}" "; "
+    . map prettyTokenValue
     . Pl.toList
     . Pl.getValue
 
 -- Unsafe
-showDatum ::
+-- TODO Consider also using `prettyprinter` lib for datum
+prettyDatum ::
   (Show a, Pl.UnsafeFromData a) =>
   -- | Proxy carrying the datum type
   Proxy a ->
   -- | Raw Plutus datum to show
   Pl.Datum ->
-  String
-showDatum proxy = show . convert proxy . Pl.getDatum
+  Doc ann
+prettyDatum proxy = Prettyprinter.pretty . show . convert proxy . Pl.getDatum
   where
     convert :: Pl.UnsafeFromData a => Proxy a -> Pl.BuiltinData -> a
     convert _proxy = Pl.unsafeFromBuiltinData
 
 -- Unsafe
-showPayload ::
+prettyPayload ::
   (Show a, Pl.UnsafeFromData a) =>
   -- | Proxy carrying the datum type
   Proxy a ->
   (Pl.Value, Maybe Pl.Datum) ->
-  String
-showPayload proxy (value, mDatum) =
-  showValue value
-    <> maybe "" ((\s -> "(" <> s <> ")") . showDatum proxy) mDatum
+  Doc ann
+prettyPayload proxy (value, mDatum) =
+  Prettyprinter.vsep
+    [ prettyValue value,
+      maybe
+        Prettyprinter.emptyDoc
+        (Prettyprinter.parens . prettyDatum proxy)
+        mDatum
+    ]
 
 -- Unsafe
-showAddress ::
+prettyAddress ::
   (Show a, Pl.UnsafeFromData a) =>
   -- | Proxy carrying the datum type
   Proxy a ->
   -- | Adress to show
   (Pl.Address, [(Pl.Value, Maybe Pl.Datum)]) ->
-  String
-showAddress proxy (address, payloads) =
-  showAddressTypeAndHash address
-    <> ":\n"
-    <> concatMap ((\s -> "- " <> s <> "\n") . showPayload proxy) payloads
+  Doc ann
+prettyAddress proxy (address, payloads) =
+  Prettyprinter.vsep
+    [ prettyAddressTypeAndHash address,
+      Prettyprinter.indent 2
+        . Prettyprinter.vsep
+        . map (("-" <>) . Prettyprinter.indent 1 . prettyPayload proxy)
+        $ payloads
+    ]
 
 -- Unsafe
-showUtxoState ::
+prettyUtxoState ::
   (Show a, Pl.UnsafeFromData a) =>
   -- | Proxy carrying the datum type
   Proxy a ->
   -- | UtxoState to show
   UtxoState ->
-  String
-showUtxoState proxy = concatMap (showAddress proxy) . Map.toList
+  Doc ann
+prettyUtxoState proxy =
+  Prettyprinter.vsep . map (prettyAddress proxy) . Map.toList
