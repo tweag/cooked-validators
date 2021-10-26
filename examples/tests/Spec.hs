0bd5b2990268ad196de03baeb233d8683d46ae70
Florent C <florent.chevrou@tweag.io>
2021-10-26 09:42:47 +0200

    Move PMultiSig & Forge to tests and add dummy test suite

--- examples/tests/Spec.hs
+++ examples/tests/Spec.hs
@@ -1,4 +1,6 @@
 import qualified SplitSpec
+import qualified PMultiSigSpec
+import qualified ForgeSpec
 import Test.Hspec
 
 main :: IO ()
@@ -7,3 +9,5 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "'Split' contract" SplitSpec.spec
+  describe "'PMultiSig' contract" PMultiSigSpec.spec
+  describe "'Forge' contract" ForgeSpec.spec
