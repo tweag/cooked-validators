770d5baab4c1afbd0e7b0d4c27cc158b78a27167
Victor Miraldo <victor.miraldo@tweag.io>
2021-11-01 13:47:07 +0100

    Bypass tests to warm up cache

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 set -uo pipefail
 
+cd cooked-validators
+hpack
+cabal build
+exit 0
+
 ## Runs ormolu on all .hs files in a given project; sets the ormolu_ok
 ## variable to `false` in case ormolu fails. It also creates an artifact
 ## explaining the failure inside the tests folder.
