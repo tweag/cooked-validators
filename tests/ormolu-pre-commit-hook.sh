f10aeb41c250101b823cc0f11d4b88dd02cfeefc
Victor Miraldo <victor.miraldo@tweag.io>
2021-11-26 11:32:18 +0100

    The pre-commit hook had a bug where ormolu would try to format a file that as moved, instead of the destination of the move.

--- tests/ormolu-pre-commit-hook.sh
+++ tests/ormolu-pre-commit-hook.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# command taken from https://github.com/JLLeitschuh/ktlint-gradle  task addKtlintFormatGitPreCommitHook
-filesToFormat="$(git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~ /\.hs/ { print $2}')"
+# command adapted from https://github.com/JLLeitschuh/ktlint-gradle  task addKtlintFormatGitPreCommitHook
+filesToFormat="$(git --no-pager diff --name-status --no-color --cached | \
+  awk '($1 == "M" || $1 == "A") && $2 ~ /\.hs/ { print $2} $1 ~ /R/ && $3 ~ /\.hs/ { print $3 } ')"
 
 echo "files to format $filesToFormat"
 for sourceFilePath in $filesToFormat
