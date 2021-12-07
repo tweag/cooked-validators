0d573abb76a34ad49518e110b5f606e7885fbf01
0xd34df00d <0xd34df00d@gmail.com>
2021-12-07 17:14:38 -0600

    Stub instance MonadMockChain for Contract

---
+++ cooked-validators/src/Cooked/MockChain/Monad/Contract.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Cooked.MockChain.Monad.Contract where
+
+import Control.Lens (review)
+import Control.Monad
+import Cooked.MockChain.Monad
+import Data.Maybe
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Ledger.Tx as Pl
+import qualified Ledger as Pl
+import qualified Ledger.Constraints as Pl
+import qualified Ledger.Credential as Pl
+import qualified Plutus.Contract as C
+import qualified Plutus.Contract.Request as C
+import qualified Plutus.Contract.Types as C
+import qualified PlutusTx as Pl
+
+-- TODO shall MonadFail really be the constraint on the MonadMockChain class?
+instance (C.AsContractError e) => MonadFail (C.Contract w s e) where
+  fail = C.throwError . review C._OtherError . T.pack
+
+instance (C.AsContractError e) => MonadMockChain (C.Contract w s e) where
+  validateTxSkel = _
+
+  utxosSuchThat addr datumPred = do
+    allUtxos <- M.toList <$> C.utxosAt addr
+    maybeUtxosWithDatums <- forM allUtxos $ \utxo -> do
+      let (Pl.TxOut _ val datumHash) = Pl.toTxOut $ snd utxo
+      datum <- maybe (pure Nothing) C.datumFromHash datumHash
+      let typedDatum = datum >>= Pl.fromBuiltinData . Pl.getDatum
+      pure $ if datumPred typedDatum val
+                then Just (utxo, typedDatum)
+                else Nothing
+    pure $ catMaybes maybeUtxosWithDatums
+
+  txOutByRef ref = fmap Pl.toTxOut <$> C.txOutFromRef ref
+
+  slotCounter = _
+
+  modifySlotCounter = _
