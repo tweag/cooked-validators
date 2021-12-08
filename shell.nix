50dd88fd232e42b4d9c591b2ef866eb39513b549
Victor Miraldo <victor.miraldo@tweag.io>
2021-12-08 14:30:45 +0100

    Attempt to use haskell.nix, no success

--- shell.nix
+++ shell.nix
@@ -1,25 +1,45 @@
-{ pkgs ? import (import ./nix/sources.nix).nixpkgs {} }:
-pkgs.mkShell {
-    buildInputs = with pkgs; [
-        # libs
-        libsodium
-        lzma
-        zlib
-
-        # required to build in a pure nix shell
-        git
-        cacert # git SSL
-        pkg-config # required by libsystemd-journal
-        systemd.dev
-
-        # build haskell
-        haskell.compiler.ghc8104
-        haskellPackages.cabal-install
-
-        # devtools
-        haskell-language-server
-        hlint
-        ormolu
-        hpack
-    ];
+(import ./default.nix).shellFor {
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "latest";
+    haskell-language-server = "latest";
+    hpack = "latest";
+    ormolu = "latest";
+  };
 }
+
+# { 
+#   sources ? import ./nix/sources.nix {},
+#   haskellNix ? import sources.haskellNix {},
+#   pkgs ? import
+#     # haskell.nix provides access to the nixpkgs pins which are used by our CI,
+#     # hence you will be more likely to get cache hits when using these.
+#     # But you can also just use your own, e.g. '<nixpkgs>'.
+#     haskellNix.sources.nixpkgs-unstable
+#     # These arguments passed to nixpkgs, include some patches and also
+#     # the haskell.nix functionality itself as an overlay.
+#     haskellNix.nixpkgsArgs
+# }:
+# pkgs.mkShell {
+#     buildInputs = with pkgs; [
+#         # libs
+#         libsodium
+#         lzma
+#         zlib
+# 
+#         # required to build in a pure nix shell
+#         git
+#         cacert # git SSL
+#         pkg-config # required by libsystemd-journal
+#         systemd.dev
+# 
+#         # build haskell
+#         haskellPackages.cabal-install
+# 
+#         # devtools
+#         haskell-language-server
+#         hlint
+#         ormolu
+#         hpack
+#     ];
+# }
