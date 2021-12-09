d1863210d8581627ac149f858d7b4cf77979af9b
Guillaume Genestier <guillaume.genestier@tweag.io>
2021-12-09 16:18:51 +0100

    Victor's suggestion

--- cooked-validators/src/Cooked/Tx/Constraints/Pretty.hs
+++ cooked-validators/src/Cooked/Tx/Constraints/Pretty.hs
@@ -47,17 +47,11 @@ prettyConstraint (PaysPK pkh val) =
 prettyConstraint (SpendsPK out) =
   let (ppAddr, mppVal) = prettyTxOut $ Pl.toTxOut $ snd out
    in prettyEnum "SpendsPK" "-" $ catMaybes [Just ppAddr, mppVal]
-prettyConstraint (Mints Nothing policies val) =
+prettyConstraint (Mints mr policies val) =
   prettyEnum "Mints" "-" $
     catMaybes
       [ mPrettyValue val,
-        Just $ "Policies:" <+> PP.list (map prettyMintingPolicy policies)
-      ]
-prettyConstraint (Mints (Just r) policies val) =
-  prettyEnum "Mints" "-" $
-    catMaybes
-      [ mPrettyValue val,
-        Just $ "With the redeemer" <+> prettyDatum r,
+        fmap (("Redeemer:" <+>) . prettyDatum) mr,
         Just $ "Policies:" <+> PP.list (map prettyMintingPolicy policies)
       ]
 prettyConstraint (SignedBy ws) =
