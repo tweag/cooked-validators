10639896b4f0481f9b67183edac9dddfb29dd688
Victor Miraldo <victor.miraldo@tweag.io>
2021-12-09 13:33:34 +0100

    Comments to shell-nix; add the needed patch

--- shell.nix
+++ shell.nix
@@ -1,21 +1,24 @@
 { 
   sources ? import ./nix/sources.nix {},
+
+  # Bring in our pinned nixpkgs, but also brings in iohk's modiied nixpkgs
+  # which contains the precious ghc810420210212 needed for compiling plutus.
   rawpkgs ? import sources.nixpkgs {},
   haskellNix ? import sources.haskellNix {},
   iohkpkgs ? import
-    # haskell.nix provides access to the nixpkgs pins which are used by our CI,
-    # hence you will be more likely to get cache hits when using these.
-    # But you can also just use your own, e.g. '<nixpkgs>'.
     haskellNix.sources.nixpkgs-unstable
-    # These arguments passed to nixpkgs, include some patches and also
-    # the haskell.nix functionality itself as an overlay.
     haskellNix.nixpkgsArgs
 }:
 let
-  my-hls = (iohkpkgs.haskell-nix.hackage-package {
+  # The only difficulty we face is making sure to build the haskell-language-server
+  # with the same version of GHC that is used for plutus; doing so, however,
+  # requires patching ghcide, a dependency of haskell-language-server.
+  #
+  # To do that, we create a hackage-package and specify a patch
+  # inside its 'modules' key:
+  custom-hls = (iohkpkgs.haskell-nix.hackage-package {
     compiler-nix-name = "ghc810420210212";
     name = "haskell-language-server";
-    # configureArgs = "--constraint \"ghcide < 1.5\"";
     version = "1.5.1.0";
     modules = [{
           packages.ghcide.patches = [ nix/patches/ghcide_partial_iface.patch ];
@@ -24,7 +27,7 @@ let
   }).components.exes.haskell-language-server;
 in rawpkgs.mkShell {
     buildInputs = with rawpkgs; [
-        # libs
+        # libs required to build plutus
         libsodium
         lzma
         zlib
@@ -34,9 +37,15 @@ in rawpkgs.mkShell {
         cacert # git SSL
         pkg-config # required by libsystemd-journal
         systemd.dev
+
+        # haskell development tools pulled from regular nixpkgs
+        hpack
+        hlint
+        ormolu
      ] ++ [
+        # iohk-specific stuff that we require
         iohkpkgs.haskell-nix.internal-cabal-install
         iohkpkgs.haskell-nix.compiler.ghc810420210212
-        my-hls
+        custom-hls
      ];
 }
