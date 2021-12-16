285771fb29723595c9197a79c9e89a313a4ebd93
0xd34df00d <0xd34df00d@gmail.com>
2021-12-16 13:49:31 -0500

    Update and fix balance tests

--- cooked-validators/tests/Cooked/BalanceSpec.hs
+++ cooked-validators/tests/Cooked/BalanceSpec.hs
@@ -1,11 +1,11 @@
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Cooked.BalanceSpec (spec) where
@@ -16,31 +16,32 @@ import Cooked.MockChain
 import Cooked.Tx.Balance
 import Cooked.Tx.Constraints
 import qualified Data.ByteString.Char8 as BS
-import qualified Data.Map as M
 import Data.Kind
+import qualified Data.Map as M
 import Data.String
 import qualified Ledger.Crypto as Pl
 import qualified Ledger.Index as Pl
+import qualified Ledger.Value as Pl
 import qualified Plutus.V1.Ledger.Ada as Pl
+import qualified Plutus.V1.Ledger.Api as Pl
 import qualified Plutus.V1.Ledger.Crypto as Pl
 import qualified Plutus.V1.Ledger.Tx as Pl
 import qualified PlutusTx.AssocMap as Map
 import qualified PlutusTx.Builtins.Internal as PlI
 import Test.Hspec
-import qualified Wallet.Emulator.Wallet as Pl
-import qualified Ledger.Value as Pl
 import Test.QuickCheck
-import qualified Plutus.V1.Ledger.Api as Pl
+import qualified Wallet.Emulator.Wallet as Pl
 
-newtype MockBalancable = MockBalancable { getMBValue :: Pl.Value }
+newtype MockBalancable = MockBalancable {getMBValue :: Pl.Value}
   deriving (Eq, Show)
 
 instance BalancableOut MockBalancable where
   type BOutRef MockBalancable = MockBalancable
   outValue = getMBValue
+  outRef = id
 
 strings :: Int -> [Pl.BuiltinByteString]
-strings m = [PlI.BuiltinByteString $ BS.singleton c | c <- take m ['a'..]]
+strings m = [PlI.BuiltinByteString $ BS.singleton c | c <- take m ['a' ..]]
 
 instance Arbitrary Pl.CurrencySymbol where
   arbitrary = Pl.CurrencySymbol <$> elements (strings 5)
@@ -51,7 +52,6 @@ instance Arbitrary Pl.TokenName where
 instance (Arbitrary k, Arbitrary v) => Arbitrary (Map.Map k v) where
   arbitrary = Map.fromList <$> arbitrary
 
-
 class ArbitraryMod (cntMod :: Type -> Type) where
   unwrap :: cntMod a -> a
 
@@ -61,29 +61,30 @@ instance ArbitraryMod Identity where
 instance ArbitraryMod Positive where
   unwrap = getPositive
 
-newtype ValueWithMods (cntMod :: Type -> Type) = ValueWithMods { vwmValue :: Pl.Value }
+newtype ValueWithMods (cntMod :: Type -> Type) = ValueWithMods {vwmValue :: Pl.Value}
   deriving newtype (Eq, Show)
 
 instance (ArbitraryMod cntMod, Arbitrary (cntMod Integer)) => Arbitrary (ValueWithMods cntMod) where
   arbitrary = do
     symsCount <- chooseInt (0, 5)
-    syms <- fmap concat $ replicateM symsCount $ do
-      sym <- arbitrary
+    syms <- fmap concat $
+      replicateM symsCount $ do
+        sym <- arbitrary
 
-      toksCount <- chooseInt (0, 5)
-      symToks <- replicateM toksCount $ do
-        tok <- arbitrary
-        cnt <- unwrap @cntMod <$> arbitrary
-        pure (tok, cnt)
+        toksCount <- chooseInt (0, 5)
+        symToks <- replicateM toksCount $ do
+          tok <- arbitrary
+          cnt <- unwrap @cntMod <$> arbitrary
+          pure (tok, cnt)
 
-      pure [Pl.singleton sym tok cnt | (tok, cnt) <- symToks]
+        pure [Pl.singleton sym tok cnt | (tok, cnt) <- symToks]
     pure $ ValueWithMods $ mconcat syms
 
 instance Arbitrary MockBalancable where
   arbitrary = MockBalancable . vwmValue @Positive <$> arbitrary
 
 allUtxosCurrsToks :: [MockBalancable] -> [(Pl.CurrencySymbol, Pl.TokenName)]
-allUtxosCurrsToks bs = [ (curr, tok) | (curr, tok, _) <- Pl.flattenValue $ foldMap getMBValue bs ]
+allUtxosCurrsToks bs = [(curr, tok) | (curr, tok, _) <- Pl.flattenValue $ foldMap getMBValue bs]
 
 spec :: SpecWith ()
 spec = do
@@ -108,22 +109,30 @@ spec = do
         leftovers `shouldBe` mempty
     it "if a (curr, tok) pair is unbalanced, it's used" $
       property $ \(ValueWithMods diff :: ValueWithMods Positive) utxos -> do
-        let utxosCurrsToks = allUtxosCurrsToks $ fst <$> utxos
+        let utxosCurrsToks = allUtxosCurrsToks utxos
             (usedUtxos, _) = spendValueFrom diff utxos
             usedUtxosCurrsToks = allUtxosCurrsToks usedUtxos
         forM_ (Pl.flattenValue diff) $ \(curr, tok, _) -> do
           when ((curr, tok) `elem` utxosCurrsToks) $
             (curr, tok) `shouldSatisfy` (`elem` usedUtxosCurrsToks)
-    xit "pointwise sum of spent and leftover" $
+    it "pointwise sum of spent and leftover" $
       -- If @(usedUtxos, leftovers) = spendValueFrom diff utxos@, then,
       -- for each (curr, token) in @usedUtxos@,
+      -- if there's enough of that pair in utxos, then
       -- @usedUtxos ! (curr, token) = diff ! (curr, token) + leftovers ! (curr, token)@.
+      -- otherwise @usedUtxos ! (curr, token) = utxos ! (curr, token)@.
+      --
+      -- TODO this latter case is not necessarily a must-have for our implementation.
       property $ \(ValueWithMods diff :: ValueWithMods Positive) utxos -> do
         let (usedUtxos, leftovers) = spendValueFrom @MockBalancable diff utxos
             usedUtxosTotal = Pl.flattenValue $ foldMap getMBValue usedUtxos
+            allUtxosValue = foldMap getMBValue utxos
         forM_ usedUtxosTotal $ \(curr, token, utxoVal) -> do
           let rhsVal = Pl.valueOf (diff <> leftovers) curr token
-          (diff, leftovers, usedUtxosTotal) `shouldSatisfy` const (rhsVal == utxoVal)
+              availableVal = Pl.valueOf allUtxosValue curr token
+          if availableVal >= Pl.valueOf diff curr token
+            then (leftovers, usedUtxosTotal) `shouldSatisfy` const (utxoVal == rhsVal)
+            else (leftovers, usedUtxosTotal) `shouldSatisfy` const (utxoVal == availableVal)
 
 outsOf :: Int -> Pl.UtxoIndex -> [(Pl.TxOutRef, Pl.TxOut)]
 outsOf i utxoIndex =
