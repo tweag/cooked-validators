b24ffa415c678cb70822c4db355b301fabf4e112
Victor Miraldo <victor.miraldo@tweag.io>
2021-12-28 12:13:11 +0100

    Will I see a cabal update?

---
+++ cooked-validators/src/AnotherExample.hs
@@ -0,0 +1,18 @@
+module AnotherExample where
+
+import Control.Monad
+import Cooked.MockChain
+import Cooked.Tx.Constraints
+import qualified Ledger.Ada as Pl
+
+-- * MockChain Example
+
+-- | Start from the initial 'UtxoIndex', where each known 'wallet's have the
+-- same amount of Ada, then transfers 4200 lovelace from wallet 1 to wallet 2
+example :: Either MockChainError ((), UtxoState)
+example = runMockChain $ do
+  void $
+    validateTxSkel $
+      txSkel
+        (wallet 1)
+        [PaysPK (walletPKHash $ wallet 2) (Pl.lovelaceValueOf 4200)]
