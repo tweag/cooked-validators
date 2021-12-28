fbc2475dcbdce8b25c50d1ef01ca0ae3c0263279
Victor Miraldo <victor.miraldo@tweag.io>
2021-12-28 12:53:09 +0100

    Is itworking now?

--- ci/commit-and-push-cabal-changes.sh
+++ ci/commit-and-push-cabal-changes.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
+
+if [[ "$#" -ne "1" ]]; then
+  echo "I need a branch name to checkout in CI"
+  exit 0
+fi
+
 changes=$(git status --porcelain | grep "^ M" | grep "\.cabal$" | sed 's/ M //')
 if [[ ! -z "$changes" ]]; then
   git config user.name github-actions
   git config user.email github-actions@github.com
+  git checkout $1
   git add $changes
   git commit -m "Generated cabal files"
   git push
