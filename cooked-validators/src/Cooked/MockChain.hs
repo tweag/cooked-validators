b6564c6365c7c8629a0644fc61016fef1eea18fe
GuillaumeGen <GuillaumeGen@users.noreply.github.com>
2022-03-02 12:00:04 +0100

    Adding a slotConfig in MonadMockChain and the function validateNow (#83)
    * Adding a slotConfig in MonadMockChain and the function validateNow
    
    * Victor's coments
    
    * Re-exporting MockChain.Constraints
    
    Co-authored-by: Guillaume Genestier <guillaume.genestier@tweag.io>

--- cooked-validators/src/Cooked/MockChain.hs
+++ cooked-validators/src/Cooked/MockChain.hs
@@ -3,7 +3,8 @@
 -- | Exports all the machinery necessary for using 'MonadBlockChain' and
 --  'MonadMockChain' for writing and testing contracts.
 module Cooked.MockChain
-  ( module Cooked.MockChain.Time,
+  ( module Cooked.MockChain.Constraints,
+    module Cooked.MockChain.Time,
     module Cooked.MockChain.UtxoState,
     module Cooked.MockChain.UtxoState.Testing,
     module Cooked.MockChain.Wallet,
@@ -14,11 +15,10 @@ module Cooked.MockChain
     module Cooked.MockChain.RawUPLC,
     module Cooked.MockChain.Testing,
     SpendableOut,
-    spentByPK,
   )
 where
 
-import Control.Arrow (second)
+import Cooked.MockChain.Constraints
 import Cooked.MockChain.Monad
 import Cooked.MockChain.Monad.Contract ()
 import Cooked.MockChain.Monad.Direct
@@ -29,16 +29,4 @@ import Cooked.MockChain.Time
 import Cooked.MockChain.UtxoState
 import Cooked.MockChain.UtxoState.Testing
 import Cooked.MockChain.Wallet
-import Cooked.Tx.Balance
-import Cooked.Tx.Constraints (Constraint (..), SpendableOut, paysPK)
-import qualified Ledger as Pl
-
--- | Spends some value from a pubkey by selecting the needed utxos belonging
---  to that pubkey and returning the leftover to the same pubkey.
---  This function is here to avoid an import cycle.
-spentByPK :: MonadBlockChain m => Pl.PubKeyHash -> Pl.Value -> m [Constraint]
-spentByPK pkh val = do
-  -- TODO: maybe turn spentByPK into a pure function: spentByPK val <$> pkUtxos
-  allOuts <- pkUtxos pkh
-  let (toSpend, leftOver, _) = spendValueFrom val $ map (second Pl.toTxOut) allOuts
-  (paysPK pkh leftOver :) . map SpendsPK <$> mapM spendableRef toSpend
+import Cooked.Tx.Constraints.Type (SpendableOut)
