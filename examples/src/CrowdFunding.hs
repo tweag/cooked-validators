1be5f120d7bd142399e23ad7ce5bf5028a564594
mmontin <mathieu.montin@enseeiht.fr>
2022-03-17 10:50:54 +0100

    Fixed imports, and reframed findDatum

--- examples/src/CrowdFunding.hs
+++ examples/src/CrowdFunding.hs
@@ -22,6 +22,7 @@ module CrowdFunding where
 import qualified Ledger
 import qualified Ledger.Contexts as Contexts
 import qualified PlutusTx
+import qualified Ledger.Ada as Ada
 import PlutusTx.Prelude hiding (Applicative (..))
 
 -- Some basic types
@@ -29,101 +30,94 @@ import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics (Generic)
 import qualified Prelude as Haskell
 
-{-
-The datum for the CrowdFunding smart contract. A peer can either :
+{- The datum for the CrowdFunding smart contract. A peer can either :
 * propose a project with its address, an id, a goal and a time range
-* contribute to a project with its address and an id
--}
+* contribute to a project with its address and an id -}
 data CrowdFundingDatum =
   ProjectProposal Ledger.PubKeyHash BuiltinByteString Integer Ledger.POSIXTimeRange |
-Funding Ledger.PubKeyHash BuiltinByteString
+  Funding Ledger.PubKeyHash BuiltinByteString
   deriving stock    (Haskell.Eq, Haskell.Show, Generic)
   deriving anyclass (ToJSON, FromJSON)
 
 -- Some magic so that Plutus understands that CrowdFundingDatum is supposed to be used as a Datum
 PlutusTx.unstableMakeIsData ''CrowdFundingDatum
 
-{-# INLINEABLE findDatumByHash #-}
-findDatumByHash :: Contexts.TxInfo -> Ledger.DatumHash -> Maybe CrowdFundingDatum
-findDatumByHash txInfo hash = do
+-- Retrieving a CrowdFundingDatum of a given input in a transaction
+findDatum :: Ledger.TxInfo -> Ledger.TxInInfo -> Maybe CrowdFundingDatum
+findDatum txInfo txInInfo = do
+  let txOut = Ledger.txInInfoResolved txInInfo
+  hash <- Ledger.txOutDatumHash txOut
   Ledger.Datum d <- Contexts.findDatum hash txInfo
   PlutusTx.fromBuiltinData d
 
-{-# INLINEABLE findDatum #-}
-findDatum :: Ledger.TxInfo -> Ledger.TxInInfo -> Maybe CrowdFundingDatum
-findDatum txInfo txInInfo =
-  Ledger.txOutDatumHash (Ledger.txInInfoResolved txInInfo) >>= findDatumByHash txInfo
+{- The redeemer for the CrowdFunding smart contract. The project can either be :
+* launched, with the following parameters :
+  - an AssetClass : the nature of the tokens to give to contributors
+  - a power of 10 : the number of parts in which the project shall be split among contributors
+* cancelled -}
+data CrowdFundingRedeemer =
+  Launch Ledger.AssetClass Integer |
+  Cancel
+  deriving stock (Haskell.Eq, Haskell.Show, Generic)
+  deriving anyclass (ToJSON, FromJSON)  
 
--- {-
--- The redeemer for the CrowdFunding smart contract. The project can either be :
--- * launched, with the following parameters :
---   - an AssetClass : the nature of the tokens to give to contributors
---   - a power of 10 : the number of parts in which the project shall be split among contributors
--- * cancelled
--- -}
--- data CrowdFundingRedeemer =
---   Launch AssetClass Integer |
---   Cancel
---   deriving stock (Eq, Show, Generic)
---   deriving anyclass (ToJSON, FromJSON)  
+{-# INLINEABLE getCurrentValue #-}
+-- Retrieves the value for the current input
+getCurrentValue :: Contexts.ScriptContext -> Maybe Ledger.Value
+getCurrentValue = (fmap (Ledger.txOutValue . Contexts.txInInfoResolved)) . Contexts.findOwnInput
 
--- {-# INLINEABLE getCurrentValue #-}
--- -- Retrieves the value for the current input
--- getCurrentValue :: ScriptContext -> Maybe Value
--- getCurrentValue = (fmap (txOutValue . txInInfoResolved)) . findOwnInput
+{-# INLINEABLE validateTimeRange #-}
+-- Retrieves the current transition time range and compares it to the project time range
+validateTimeRange :: Ledger.POSIXTimeRange -> Contexts.ScriptContext -> Bool
+validateTimeRange range =
+  (== range) .
+  (\/ range) .
+  Contexts.txInfoValidRange .
+  Contexts.scriptContextTxInfo
 
--- {-# INLINEABLE validateTimeRange #-}
--- -- Retrieves the current transition time range and compares it to the project time range
--- validateTimeRange :: Ledger.POSIXTimeRange -> ScriptContext -> Bool
--- validateTimeRange range =
---   (== range) .
---   (\/ range) .
---   txInfoValidRange .
---   scriptContextTxInfo
+{-# INLINEABLE validateTotalSum #-}
+-- The sum of all input lovelaces should be greater than the threshold
+validateTotalSum :: Integer -> Contexts.ScriptContext -> Bool
+validateTotalSum threshold =
+  (<= threshold) .
+  Ada.getLovelace .
+  Ada.fromValue .
+  mconcat .
+  fmap (Contexts.txOutValue . Contexts.txInInfoResolved) .
+  Contexts.txInfoInputs .
+  Contexts.scriptContextTxInfo
 
--- {-# INLINEABLE validateTotalSum #-}
--- -- The sum of all input lovelaces should be greater than the threshold
--- validateTotalSum :: Integer -> ScriptContext -> Bool
--- validateTotalSum threshold =
---   (<= threshold) .
---   getLovelace .
---   fromValue .
---   mconcat .
---   fmap (txOutValue . txInInfoResolved) .
---   txInfoInputs .
---   scriptContextTxInfo
+{-# INLINEABLE isProjectProposal #-}
+-- Checks if the given Datum corresponds to a project proposal
+isProjectProposal :: Maybe CrowdFundingDatum -> Bool
+isProjectProposal (Just (ProjectProposal _ _ _ _)) = True
+isProjectProposal _ = False
 
--- {-# INLINEABLE isProjectProposal #-}
--- -- Checks if the given Datum corresponds to a project proposal
--- isProjectProposal :: Maybe CrowdFundingDatum -> Bool
--- isProjectProposal (Just (ProjectProposal _ _ _ _)) = True
--- isProjectProposal _ = False
-
--- {-# INLINEABLE validateSingleProposal #-}
--- -- Checks that the context only contains a single instance of a project proposal
--- validateSingleProposal :: ScriptContext -> Bool
--- validateSingleProposal = undefined
---   -- (== 1) .
---   -- length .
---   -- (filter isProjectProposal) .
---   -- (fmap (PlTx.fromBuiltinData . snd)) .
---   -- txInfoData .
---   -- scriptContextTxInfo
+{-# INLINEABLE validateSingleProposal #-}
+-- Checks that the context only contains a single instance of a project proposal
+validateSingleProposal :: Contexts.ScriptContext -> Bool
+validateSingleProposal = Haskell.undefined
+  -- (== 1) .
+  -- length .
+  -- (filter isProjectProposal) .
+  -- (fmap (PlTx.fromBuiltinData . snd)) .
+  -- txInfoData .
+  -- scriptContextTxInfo
   
--- {-# INLINEABLE validateCrowdFunding #-}
--- validateCrowdFunding :: CrowdFundingDatum -> CrowdFundingRedeemer -> ScriptContext -> Bool
--- -- We can always cancel the project regarding the project proposal, or not
--- -- Make sure there is no other input belonging to the current script when we are in this case
--- validateCrowdFunding (ProjectProposal _ _ _ _) Cancel _   = True
--- -- To launch the project, we need to check if the total amount of money is enough
--- -- We also need to check whether the current time is part of the time range
--- -- Finally, we need to check if there is a single instance of a project proposal
--- validateCrowdFunding (ProjectProposal _ _ threshold range) (Launch _ _) ctx =
---   traceIfFalse "Current time not in the time range."              (validateTimeRange range ctx) &&
---   traceIfFalse "The project has not reach its funding threshold." (validateTotalSum threshold ctx) &&
---   traceIfFalse "There are multiple project proposals."            (validateSingleProposal ctx)
--- validateCrowdFunding (Funding hash id) (Launch _ _) ctx = undefined
--- validateCrowdFunding (Funding hash id) Cancel ctx = undefined
+{-# INLINEABLE validateCrowdFunding #-}
+validateCrowdFunding :: CrowdFundingDatum -> CrowdFundingRedeemer -> Contexts.ScriptContext -> Bool
+-- We can always cancel the project regarding the project proposal, or not
+-- Make sure there is no other input belonging to the current script when we are in this case
+validateCrowdFunding (ProjectProposal _ _ _ _) Cancel _   = True
+-- To launch the project, we need to check if the total amount of money is enough
+-- We also need to check whether the current time is part of the time range
+-- Finally, we need to check if there is a single instance of a project proposal
+validateCrowdFunding (ProjectProposal _ _ threshold range) (Launch _ _) ctx =
+  traceIfFalse "Current time not in the time range."              (validateTimeRange range ctx) &&
+  traceIfFalse "The project has not reach its funding threshold." (validateTotalSum threshold ctx) &&
+  traceIfFalse "There are multiple project proposals."            (validateSingleProposal ctx)
+validateCrowdFunding (Funding hash id) (Launch _ _) ctx = Haskell.undefined
+validateCrowdFunding (Funding hash id) Cancel ctx = Haskell.undefined
 
 -- data CrowdFunding
 
