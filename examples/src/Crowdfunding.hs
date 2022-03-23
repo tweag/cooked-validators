42fc1dbfa99135e5673a3e0519c87254a3346b3b
Alejandro Serrano <trupill@gmail.com>
2022-03-23 13:51:34 +0100

    First go at validators

--- examples/src/Crowdfunding.hs
+++ examples/src/Crowdfunding.hs
@@ -12,7 +12,7 @@
 {-# language TypeOperators #-}
 module Crowdfunding where
 
-import Control.Monad.Extra (concatMapM)
+import Control.Monad.Extra (guard, concatMapM)
 import Data.Aeson
 import PlutusTx.Prelude
 import qualified Prelude as Haskell
@@ -20,7 +20,8 @@ import qualified GHC.Generics as Haskell
 
 import qualified PlutusTx
 import qualified Ledger
-import Ledger.Typed.Scripts
+import qualified Ledger.Ada as Ada
+import qualified Ledger.Typed.Scripts as Script
 import Plutus.Contract
 import Schema (ToSchema)
 
@@ -30,7 +31,8 @@ import Schema (ToSchema)
 data ProjectData =
   ProjectData {
     minimalFunding :: Ledger.Ada
-  , expiryDate     :: Ledger.POSIXTimeRange
+  , expiryDate     :: Ledger.POSIXTime
+  , ownerKey       :: Ledger.PubKeyHash
   }
   deriving stock (Haskell.Show, Haskell.Generic)
   deriving anyclass (ToJSON, FromJSON, ToSchema)
@@ -47,39 +49,88 @@ data FunderRedeemer = Cancel | Fund
 concatMapM PlutusTx.unstableMakeIsData [''ProjectData, ''FundData, ''OwnerRedeemer, ''FunderRedeemer]
 concatMapM PlutusTx.makeLift [''ProjectData, ''FundData, ''OwnerRedeemer, ''FunderRedeemer]
 
+-- TRANSACTION-RELATED UTILITIES
+-- =============================
+
+{-# INLINABLE isSignedBy #-}
+isSignedBy :: Ledger.ScriptContext -> Ledger.PubKeyHash -> Bool
+ctx `isSignedBy` signer = Ledger.scriptContextTxInfo ctx `isSignedByTx` signer
+
+{-# INLINABLE isSignedByTx #-}
+isSignedByTx :: Ledger.TxInfo -> Ledger.PubKeyHash -> Bool
+txInfo `isSignedByTx` signer = signer `elem` Ledger.txInfoSignatories txInfo
+
+{-# INLINABLE findDataOf #-}
+findDataOf :: forall d. PlutusTx.FromData d => Ledger.TxInfo -> [(d, Ledger.Value)]
+findDataOf tx = do
+  let txIns = map Ledger.txInInfoResolved (Ledger.txInfoInputs tx)
+  flip mapMaybe txIns $ \txOut -> do
+    hash <- Ledger.txOutDatumHash txOut
+    Ledger.Datum d <- Ledger.findDatum hash tx
+    (,) <$> PlutusTx.fromBuiltinData d <*> pure (Ledger.txOutValue txOut)
+
+{-# INLINABLE run #-}
+run :: Maybe a -> Bool
+run = isJust
+
 -- VALIDATORS
 -- ==========
 
+{-# INLINABLE validateOwner #-}
 validateOwner :: ProjectData -> OwnerRedeemer -> Ledger.ScriptContext -> Bool
-validateOwner (ProjectData _ _) GetFunds _ = False
+validateOwner ProjectData { ownerKey } GetFunds ctx =
+  ctx `isSignedBy` ownerKey
+  -- what should be checked here?
 
+{-# INLINABLE validateFunder #-}
 validateFunder :: FundData -> FunderRedeemer -> Ledger.ScriptContext -> Bool
-validateFunder FundData { funderKey } Cancel ctx = do
-  let signers = Ledger.txInfoSignatories (Ledger.scriptContextTxInfo ctx)
-  funderKey `elem` signers
-validateFunder FundData { projectOwnerKey } Fund _ = False
+
+validateFunder FundData { funderKey } Cancel ctx =
+  ctx `isSignedBy` funderKey
+  -- use the money as you wish
+  -- since this input is consumed we know this money
+  -- is never going to be used to fund a project
+
+-- this is part of a transaction executed by the owner
+-- of the project to get the funds locked from funders
+validateFunder FundData { projectOwnerKey } Fund ctx = run $ do
+  let tx = Ledger.scriptContextTxInfo ctx
+  -- signed by the owner of the project
+  guard $ tx `isSignedByTx` projectOwnerKey
+  -- which is also the owner of the project
+  let [ (ProjectData { ownerKey, minimalFunding, expiryDate }, _) ] 
+        = findDataOf @ProjectData tx
+  guard $ ownerKey == projectOwnerKey
+  -- this UTxO is spent before the expiry date
+  guard $ expiryDate `Ledger.before` Ledger.txInfoValidRange tx
+  -- the amount should be correct
+  let funders     = findDataOf @FundData tx
+      totalAmount = Ada.fromValue $ foldMap snd funders
+  guard $ totalAmount >= minimalFunding
+  -- and all of it goes to the owner
+  guard $ Ada.fromValue (Ledger.valuePaidTo tx ownerKey) >= totalAmount
 
 data OwnerValidator
-instance ValidatorTypes OwnerValidator where
+instance Script.ValidatorTypes OwnerValidator where
   type instance RedeemerType OwnerValidator = OwnerRedeemer
   type instance DatumType    OwnerValidator = ProjectData
 
-ownerInstance :: TypedValidator OwnerValidator
-ownerInstance = mkTypedValidator @OwnerValidator
+ownerInstance :: Script.TypedValidator OwnerValidator
+ownerInstance = Script.mkTypedValidator @OwnerValidator
     $$(PlutusTx.compile [|| validateOwner ||])
     $$(PlutusTx.compile [|| wrap ||])
-    where wrap = wrapValidator
+    where wrap = Script.wrapValidator @ProjectData @OwnerRedeemer
 
 data FunderValidator
-instance ValidatorTypes FunderValidator where
+instance Script.ValidatorTypes FunderValidator where
   type instance RedeemerType FunderValidator = FunderRedeemer
   type instance DatumType    FunderValidator = FundData
 
-funderInstance :: TypedValidator FunderValidator
-funderInstance = mkTypedValidator @FunderValidator
+funderInstance :: Script.TypedValidator FunderValidator
+funderInstance = Script.mkTypedValidator @FunderValidator
     $$(PlutusTx.compile [|| validateFunder ||])
     $$(PlutusTx.compile [|| wrap ||])
-    where wrap = wrapValidator
+    where wrap = Script.wrapValidator @FundData @FunderRedeemer
 
 -- ENDPOINT
 -- ========
