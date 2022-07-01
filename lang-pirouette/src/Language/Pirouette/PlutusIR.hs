bfc1735e17a7117246413faafc62447bd14a6bad
Victor Miraldo <victor.miraldo@tweag.io>
2022-07-01 15:31:53 +0200

    Add Language.Pirouette and friends

---
+++ lang-pirouette/src/Language/Pirouette/PlutusIR.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Language.Pirouette.PlutusIR (module X, pir, pirTy) where
+
+import Language.Pirouette.PlutusIR.Runner as X
+import Language.Pirouette.PlutusIR.SMT as X
+import Language.Pirouette.PlutusIR.Syntax as X
+import Language.Pirouette.PlutusIR.ToTerm as X (trProgram, trProgramDecls)
+import Language.Pirouette.PlutusIR.Typing as X ()
+import qualified Language.Pirouette.QuasiQuoter as QQ
+
+pir :: QQ.QuasiQuoter
+pir = QQ.term @PlutusIR
+
+pirTy :: QQ.QuasiQuoter
+pirTy = QQ.ty @PlutusIR
---
+++ lang-pirouette/src/Language/Pirouette/PlutusIR/Runner.hs
@@ -0,0 +1,24 @@
+module Language.Pirouette.PlutusIR.Runner where
+
+{-
+import Pirouette
+import Language.Pirouette.PlutusIR.Syntax
+
+import qualified Ledger.Typed.Scripts as Pl
+
+pirouetteTypedValidator' :: StoppingCondition -> TypedValidator a -> AssumeProve PlutusIR -> Assertion
+pirouetteTypedValidator' stop tval cond = do
+  let pir = Pl.getPir tval
+  undefined
+
+-}
+
+{-
+Hook options to tasty:
+
+data PirouetteStoppingCondition = PirouetteStoppingCondition StoppingCondition
+  deriving (Typeable)
+
+instance IsOption PirouetteStoppingCondition where
+  ...
+-}
