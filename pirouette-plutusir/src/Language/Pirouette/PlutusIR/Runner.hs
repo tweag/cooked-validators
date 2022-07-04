35ecdc9bf8e6cdf86ed60d3336670402a8caef2e
Victor Miraldo <victor.miraldo@tweag.io>
2022-07-04 13:48:27 +0200

    Found the culprit! PlutusIR translation was translating type variables and term variables with separate de bruijn indices, we need to choose one representation and stick to it

--- pirouette-plutusir/src/Language/Pirouette/PlutusIR/Runner.hs
+++ pirouette-plutusir/src/Language/Pirouette/PlutusIR/Runner.hs
@@ -52,7 +52,7 @@ pirouetteCompiledCode' stop code (PrtUnorderedDefs augments) (toAssume :==>: toP
   let augments' = runReader (contextualizeDecls augments) (PrtUnorderedDefs decls0)
   -- Now we try to remove as many 'nameUnique' bits as possible, to help with writing
   -- predicates and referring to types.
-  let (decls1, main) = declsUniqueNames (augments' `M.union` decls0) main0
+  let (decls1, main) = (augments' `M.union` decls0, main0)
 
   -- PrtUnorderedDefs decls1 = prenex $ PrtUnorderedDefs decls0'
   -- TODO: what if we don't manage to infer the type of main? Also, can't we make this interface much better? These empty lists are awkward here.
@@ -122,8 +122,14 @@ gogogo toDo prog0 (IncorrectnessParams fn ty (assume :==>: toProve)) = do
   -- us to translate it to SMTLIB later on
   liftIO (writeFile "prog0" $ show $ pretty $ prtUODecls prog0)
   let prog1 = monomorphize prog0
+  case typeCheckDecls (prtUODecls prog1) of
+    Left tyerr -> liftIO $ assertFailure $ ("prog1: " ++) $ show $ pretty tyerr
+    Right _ -> liftIO (putStrLn "monomorphize type checks...")
   liftIO (writeFile "prog1-mono" $ show $ pretty $ prtUODecls prog1)
   let prog2 = defunctionalize prog1
+  case typeCheckDecls (prtUODecls prog1) of
+    Left tyerr -> liftIO $ assertFailure $ ("prog2: " ++) $ show $ pretty tyerr
+    Right _ -> liftIO (putStrLn "defunctionalizaton type checks...")
   liftIO (writeFile "prog2-defun" $ show $ pretty $ prtUODecls prog2)
   let prog3 = elimEvenOddMutRec prog2
   liftIO (writeFile "prog3-ord" $ unlines $ map show $ prtDepOrder prog3)
