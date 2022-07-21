6e1acefa9e059c1a8e606c2f1550d03cf0280b87
Facundo Domínguez <facundo.dominguez@tweag.io>
2022-07-21 14:06:46 -0300

    Changes from code review with Victor Miraldo
    * Rename Bid to Bet
    * Use valuePaidTo

--- examples/src/BettingGame.hs
+++ examples/src/BettingGame.hs
@@ -15,18 +15,18 @@
 {-# OPTIONS_GHC -fno-omit-interface-pragmas #-}
 {-# OPTIONS_GHC -fno-specialise #-}
 
--- | This module implements a bidding game.
+-- | This module implements a betting game.
 --
--- Every game has a deadline for bids, a deadline for collecting the
--- bids, and a deadline for publishing the game results.
+-- Every game has a deadline for bets, a deadline for collecting the
+-- bets, and a deadline for publishing the game results.
 --
 -- The expected flow is as follows:
 --
--- 1. Operator starts a bidding game
--- 2. Players can place bids until the bidding deadline expires
--- 3. The operator collects the bids after the bidding deadline
---    and before the bid-collecting deadline.
--- 4. The operator publishes the game result after the bid-collecting
+-- 1. Operator starts a betting game
+-- 2. Players can place bets until the betting deadline expires
+-- 3. The operator collects the bets after the betting deadline
+--    and before the bet-collecting deadline.
+-- 4. The operator publishes the game result after the bet-collecting
 --    deadline and before the publishing deadline, and it distributes
 --    the money among the winners
 -- 5. Players are able to get their money back if the operator
@@ -35,21 +35,21 @@
 --
 -- The operator can only close the game once. In particular, she
 -- shouldn't be able to close the game more than once with
--- different sets of bids.
+-- different sets of bets.
 --
--- Bids should be possible to do in parallel. Note that this
+-- Bets should be possible to do in parallel. Note that this
 -- requirement makes impossible:
--- * to guarantee that the operator doesn't leave any bids unconsumed.
--- * to stop bidders from bidding more than once.
+-- * to guarantee that the operator doesn't leave any bets unconsumed.
+-- * to stop players from betting more than once.
 --
--- The operator must collect the bids ahead of knowing the result
--- of the game. Otherwise, she could chose to only collect the bids of
+-- The operator must collect the bets ahead of knowing the result
+-- of the game. Otherwise, she could chose to only collect the bets of
 -- the losers and never pay any money back to the winners.
 --
 -- If a player bets more than once, the on-chain validation doesn't ensure
 -- that the operator pays her for all her winning bets.
 --
-module BiddingGame where
+module BettingGame where
 
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics (Generic)
@@ -71,143 +71,141 @@ import qualified Prelude as Haskell
 -- Contract flow
 --
 -- * The operator creates an output to the script with datum @GameStart@
--- * The bidders pay to the script with datum @Bid GameResult@
--- * The operator collects the bids and pays to the script with all of
---   the collected money and datum @CollectedBids _bids@.
+-- * The betders pay to the script with datum @Bet GameResult@
+-- * The operator collects the bets and pays to the script with all of
+--   the collected money and datum @CollectedBets _bets@.
 -- * The operator publishes the result and pays to the winners.
 --
--- If the operator does not collect, players reclaim their individual bids.
+-- If the operator does not collect, players reclaim their individual bets.
 -- If the operator does not publish the result, anyone can return the
--- collected bids to all players.
+-- collected bets to all players.
 
-data BidParams = BidParams
-  { biddingDeadline :: Ledger.POSIXTime
+data BetParams = BetParams
+  { bettingDeadline :: Ledger.POSIXTime
   , collectionDeadline :: Ledger.POSIXTime
   , publishingDeadline :: Ledger.POSIXTime
-  , minimumBid :: Ledger.Value
+  , minimumBet :: Ledger.Value
   , description :: BuiltinByteString
-  , operator :: Ledger.PaymentPubKeyHash
+  , operator :: Ledger.PubKeyHash
   }
   deriving stock (Haskell.Show, Haskell.Eq, Generic)
   deriving anyclass (ToJSON, FromJSON, ToSchema)
 
 type GameResult = Bool
 
-data BidData = BidData
-  { bidder :: Ledger.PaymentPubKeyHash
+data BetData = BetData
+  { player :: Ledger.PubKeyHash
   , gameResult :: GameResult
   , amount :: Ledger.Value
   }
   deriving stock (Haskell.Show, Haskell.Eq, Generic)
 
-instance Eq BidData where
-  BidData a b c == BidData a' b' c' = a == a' && b == b' && c == c'
+instance Eq BetData where
+  BetData a b c == BetData a' b' c' = a == a' && b == b' && c == c'
 
-data BidDatum
-  = GameStart BidParams
-  | Bid BidData
-  | -- | Contains the bidders, with duplicates if they bid more than once.
-    CollectedBids [BidData]
+data BetDatum
+  = GameStart BetParams
+  | Bet BetData
+  | -- | Contains the players, with duplicates if they bet more than once.
+    CollectedBets [BetData]
   deriving stock (Haskell.Show, Haskell.Eq, Generic)
 
-data BidRedeemer
-  = BidCollection [BidData]
+data BetRedeemer
+  = BetCollection [BetData]
   | GameClose GameResult
-  | BidReclaim
+  | BetReclaim
 
 minLovelace :: Integer
 minLovelace = 2000000
 
-validateBid
-  :: BidParams -> BidDatum -> BidRedeemer -> ScriptContext -> Bool
-validateBid p d0 r ctx =
+validateBet
+  :: BetParams -> BetDatum -> BetRedeemer -> ScriptContext -> Bool
+validateBet p d0 r ctx =
     let info :: Ledger.TxInfo
         info = Ledger.scriptContextTxInfo ctx
 
         outputs = Ledger.txInfoOutputs info
 
-        outputHasCollectedBids bids =
+        outputHasCollectedBets bets =
           case Ledger.findDatumHash
-                 (Api.Datum $ PlutusTx.toBuiltinData $ CollectedBids bids)
+                 (Api.Datum $ PlutusTx.toBuiltinData $ CollectedBets bets)
                  info
             of
             Just h ->
               any (\o ->    Just h == Ledger.txOutDatumHash o
-                         && Ledger.txOutValue o == sum (map amount bids)
+                         && Ledger.txOutValue o == sum (map amount bets)
                   )
                   outputs
             Nothing -> False
 
-        paysTo h v =
-          let oAddress = Ledger.pubKeyHashAddress h Nothing
-           in any (\o -> oAddress == Ledger.txOutAddress o && Ledger.txOutValue o == v) outputs
+        paysTo h v = Ledger.valuePaidTo info h == v
 
-        paysToAllWinners gr bids =
-          let winners = filter ((gr ==) . gameResult) bids
+        paysToAllWinners gr bets =
+          let winners = filter ((gr ==) . gameResult) bets
               total_winners = sum (map amount winners)
-              commission = Ada.lovelaceValueOf (1000000 * length bids)
-              total = sum (map amount bids) - commission
+              commission = Ada.lovelaceValueOf (1000000 * length bets)
+              total = sum (map amount bets) - commission
               winnerIsPaid b =
                 if gameResult b /= gr then True
                 else
                   paysTo
-                    (bidder b)
+                    (player b)
                     (Api.unionWith Builtins.divideInteger
                        (Api.unionWith (*) total (amount b))
                        total_winners
                     )
            in
-              all winnerIsPaid bids
+              all winnerIsPaid bets
 
      in case r of
-      BidCollection bids ->
+      BetCollection bets ->
         case d0 of
           GameStart _p ->
              traceIfFalse "only the operator can collect the output of gamestart"
-               (elem (Ledger.unPaymentPubKeyHash $ operator p) $ Ledger.txInfoSignatories info)
-          Bid bid ->
+               (elem (operator p) $ Ledger.txInfoSignatories info)
+          Bet bet ->
                traceIfFalse "collection deadline expired"
                  (Ledger.to (collectionDeadline p) `Ledger.contains` Ledger.txInfoValidRange info)
             && traceIfFalse "the transaction must consume an output with datum GameStart"
                  (isJust $ findInputWithDatum (GameStart p) info)
-            && traceIfFalse "there must be an output containing the list of bids"
-                 (outputHasCollectedBids bids)
-            && traceIfFalse "the bid of this output must be included in the list of bids in the transaction output"
-                 (elem bid bids)
+            && traceIfFalse "there must be an output containing the list of bets"
+                 (outputHasCollectedBets bets)
+            && traceIfFalse "the bet of this output must be included in the list of bets in the transaction output"
+                 (elem bet bets)
           _ ->
-             traceIfFalse "BidCollection can only take outputs with GameStart and Bid datums" False
+             traceIfFalse "BetCollection can only take outputs with GameStart and Bet datums" False
       GameClose gr ->
         case d0 of
-          CollectedBids bids ->
+          CollectedBets bets ->
                traceIfFalse "publication deadline expired"
                  (Ledger.to (publishingDeadline p) `Ledger.contains` Ledger.txInfoValidRange info)
-            && traceIfFalse "bidding deadline hasn't expired"
-                 (biddingDeadline p `Ledger.before` Ledger.txInfoValidRange info)
+            && traceIfFalse "betting deadline hasn't expired"
+                 (bettingDeadline p `Ledger.before` Ledger.txInfoValidRange info)
             && traceIfFalse "the operator must earn a commission"
-                 (paysTo (operator p) (Ada.lovelaceValueOf (1000000 * length bids)))
-            && traceIfFalse "all winners must earn in proportion to what they bid"
-                 (paysToAllWinners gr bids)
+                 (paysTo (operator p) (Ada.lovelaceValueOf (1000000 * length bets)))
+            && traceIfFalse "all winners must earn in proportion to what they bet"
+                 (paysToAllWinners gr bets)
             && traceIfFalse "the transaction must be signed by the operator (so we can trust the result)"
-                 (elem (Ledger.unPaymentPubKeyHash $ operator p) $ Ledger.txInfoSignatories info)
+                 (elem (operator p) $ Ledger.txInfoSignatories info)
           _ ->
-            traceIfFalse "GameClose can only take an output with datum BidCollection" False
+            traceIfFalse "GameClose can only take an output with datum BetCollection" False
 
-      BidReclaim ->
+      BetReclaim ->
         case d0 of
-          Bid bid ->
+          Bet bet ->
                traceIfFalse "collection deadline must have expired"
                  (collectionDeadline p `Ledger.before` Ledger.txInfoValidRange info)
-            && traceIfFalse "the transaction must pay to the bidder"
-                 (paysTo (bidder bid) (amount bid))
-          CollectedBids bids ->
+            && traceIfFalse "the transaction must pay to the player"
+                 (paysTo (player bet) (amount bet))
+          CollectedBets bets ->
                traceIfFalse "publication deadline must have expired"
                  (publishingDeadline p `Ledger.before` Ledger.txInfoValidRange info)
             && traceIfFalse "the transaction must pay to all players"
-                 (all (\b -> paysTo (bidder b) (amount b)) bids)
+                 (all (\b -> paysTo (player b) (amount b)) bets)
           _ ->
-             traceIfFalse "BidReclaim can only take outputs with Bid and BidCollection datums" False
+             traceIfFalse "BetReclaim can only take outputs with Bet and BetCollection datums" False
   where
-    findInputWithDatum :: BidDatum -> Ledger.TxInfo -> Maybe Ledger.TxInInfo
+    findInputWithDatum :: BetDatum -> Ledger.TxInfo -> Maybe Ledger.TxInInfo
     findInputWithDatum d info =
       case Ledger.findDatumHash
              (Api.Datum $ PlutusTx.toBuiltinData d)
@@ -221,22 +219,22 @@ validateBid p d0 r ctx =
 
 -- Plutus boilerplate
 
-data BiddingGame
+data BettingGame
 
-PlutusTx.makeLift ''BidParams
-PlutusTx.unstableMakeIsData ''BidData
-PlutusTx.unstableMakeIsData ''BidDatum
-PlutusTx.unstableMakeIsData ''BidParams
-PlutusTx.unstableMakeIsData ''BidRedeemer
+PlutusTx.makeLift ''BetParams
+PlutusTx.unstableMakeIsData ''BetData
+PlutusTx.unstableMakeIsData ''BetDatum
+PlutusTx.unstableMakeIsData ''BetParams
+PlutusTx.unstableMakeIsData ''BetRedeemer
 
-instance Scripts.ValidatorTypes BiddingGame where
-  type RedeemerType BiddingGame = BidRedeemer
-  type DatumType BiddingGame = BidDatum
+instance Scripts.ValidatorTypes BettingGame where
+  type RedeemerType BettingGame = BetRedeemer
+  type DatumType BettingGame = BetDatum
 
-bidValidator :: BidParams -> Scripts.TypedValidator BiddingGame
-bidValidator p =
-  Scripts.mkTypedValidator @BiddingGame
-    ($$(PlutusTx.compile [||validateBid||]) `PlutusTx.applyCode` PlutusTx.liftCode p)
+betValidator :: BetParams -> Scripts.TypedValidator BettingGame
+betValidator p =
+  Scripts.mkTypedValidator @BettingGame
+    ($$(PlutusTx.compile [||validateBet||]) `PlutusTx.applyCode` PlutusTx.liftCode p)
     $$(PlutusTx.compile [||wrap||])
   where
-    wrap = Scripts.mkUntypedValidator @BidDatum @BidRedeemer
+    wrap = Scripts.mkUntypedValidator @BetDatum @BetRedeemer
