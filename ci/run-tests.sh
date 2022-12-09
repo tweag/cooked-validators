764dd6e90a58b82418091c918d40be9f0f984a26
Gabriel Hondet <7418676+gabrielhdt@users.noreply.github.com>
2022-12-09 12:22:49 +0100

    CI tweaks
    - Put tests that don't require internet (i.e. ormolu and hpack) into
      the flake checks
    - Some sanity on the script 'run-tests' (advised by shellcheck)
    - Update github actions dependencies
    - Update github caching

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+set -x # Debug CI
 set -uo pipefail
 
 show_help() {
@@ -9,8 +9,8 @@ usage: ./ci/run-tests.sh [--ci]
   Options:
     --ci      Informs the script it is running in CI; this means
               we will save the test results as a file (named <project>-cabal-test.{res,out})
-              The script WILL SUCCEED IFF THE BUILD SUCCEEDED; 
-              This behavior ensures that the build gets cached. 
+              The script WILL SUCCEED IFF THE BUILD SUCCEEDED;
+              This behavior ensures that the build gets cached.
               Another job should check the artifact/cache to decide whether
               the workflow passes or fails as a whole
 EOF
@@ -24,11 +24,11 @@ while [[ $# -ne "0" ]]; do
   esac
 done
 
-if $ci; then
-  pushd ..
-  cabal update
-  popd
-fi
+# if $ci; then
+#   pushd ..
+#   cabal update
+#   popd || exit
+# fi
 
 ## Each step below runs a certain program and, when running in CI, creates
 ## two files: ${proj}-${step}.res and ${proj}-${step}.out
@@ -38,56 +38,15 @@ fi
 ## The point of this is that when running in CI, we will execute all these steps
 ## but we "succeed" iff the build is ok. Another job will then check the resulting
 ## files for their exit codes. This is ugly but it ensures that the cabal build
-## gets cached even if tests or ormolu fails, which means we save a lot of time 
+## gets cached even if tests or ormolu fails, which means we save a lot of time
 ## (and CI runner money) in the long run.
-
-run_hpack() {
-  local proj=$1
-  echo "Running hpack on $proj"
-  local hpack_res=0
-
-  pushd "$proj"
-  hpack | grep -v "generated"
-  hpack_res=$?
-  popd
-  
-  if $ci; then
-    if [[ "$hpack_res" -ne "0" ]]; then
-      echo "hpack regenerated the cabal file; please push it!" >> "./${proj}-hpack.out"
-    else
-      echo "hpack didn't need to regenarate; all good" >> "./${proj}-hpack.out"
-    fi
-    echo "run_hpack:$hpack_res" >> "./${proj}-hpack.res"
-  fi 
-
-  return $hpack_res
-}
-
-## Runs ormolu on all .hs files in a given project; sets the ormolu_ok
-## variable to `false` in case ormolu fails. It also creates an artifact
-## explaining the failure inside the tests folder.
-run_ormolu() {
-  local proj=$1
-  echo "Running ormolu on $proj"
-  local ormolu_res=0
-  if $ci; then
-    ormolu --mode check $(find ./$proj -name '*.hs') 2> >(tee "./${proj}-ormolu.out")
-    ormolu_res=$?
-    echo "run_ormolu:$ormolu_res" >> "./${proj}-ormolu.res"
-  else 
-    ormolu --mode inplace $(find ./$proj -name '*.hs') 
-    ormolu_res=$?
-  fi 
-
-  return $ormolu_res
-}
 
 ## Runs the cabal build for a project
 run_cabal_build() {
   local proj=$1
   echo "Running 'cabal build' on $proj"
 
-  pushd "$proj"
+  pushd "$proj" || exit
   local cabal_res=0
   if $ci; then
     cabal build | tee "../${proj}-cabal-build.out"
@@ -97,7 +56,7 @@ run_cabal_build() {
     cabal build
     cabal_res=$?
   fi
-  popd
+  popd || exit
 
   return $cabal_res
 }
@@ -107,7 +66,7 @@ run_cabal_test() {
   local proj=$1
   echo "Running 'cabal run tests' on $proj"
 
-  pushd "$proj"
+  pushd "$proj" || exit
   local cabal_res=0
   if $ci; then
     cabal run tests | tee "../${proj}-cabal-test.out"
@@ -117,7 +76,7 @@ run_cabal_test() {
     cabal run tests
     cabal_res=$?
   fi
-  popd
+  popd || exit
 
   return $cabal_res
 }
@@ -125,36 +84,20 @@ run_cabal_test() {
 projects=("cooked-validators" "examples" "pirouette-plutusir")
 overall_ok=true
 
-for p in ${projects[*]}; do
-  hpack_ok=true
-  ormolu_ok=true
+for p in "${projects[@]}"; do
   cabal_build_ok=true
   cabal_test_ok=true
 
-  run_hpack "$p"
-  if [[ "$?" -ne "0" ]]; then
-    echo "[FAILURE] 'hpack' failed for $p; check the respective artifact."
-    hack_ok=false
-  fi
-
-  run_cabal_build "$p"
-  if [[ "$?" -ne "0" ]]; then
+  if ! run_cabal_build "$p"; then
     echo "[FAILURE] 'cabal build' failed for $p; check the respective artifact."
     cabal_build_ok=false
   fi
 
   if $cabal_build_ok; then
-    run_cabal_test "$p"
-    if [[ "$?" -ne "0" ]]; then
+    if ! run_cabal_test "$p"; then
       echo "[FAILURE] 'cabal run tests' failed for $p; check the respective artifact."
       cabal_test_ok=false
     fi
-  fi
-
-  run_ormolu "$p"
-  if [[ "$?" -ne "0" ]]; then
-    echo "[FAILURE] 'ormolu' failed for $p; check the respective artifact."
-    ormolu_ok=false
   fi
 
   if $ci; then
@@ -162,10 +105,10 @@ for p in ${projects[*]}; do
       overall_ok=false
     fi
   else
-    if ! ($hpack_ok && $cabal_build_ok && $cabal_test_ok && $ormolu_ok && $hpack_ok); then
+    if ! ($cabal_build_ok && $cabal_test_ok); then
       overall_ok=false
     fi
-  fi    
+  fi
 done
 
 if $overall_ok; then
--- flake.nix
+++ flake.nix
@@ -9,6 +9,22 @@
       in {
         formatter = pkgs.nixfmt;
 
+        ## `cabal test` is not run here because it requires internet
+        ## to fetch the dependencies
+        checks.default = pkgs.stdenv.mkDerivation {
+          name = "ormolu/hpack checks";
+          src = ./.;
+          buildInputs = with pkgs; [ findutils hpack ormolu ];
+          buildPhase = ''
+            hpack cooked-validators/package.yaml | grep -q 'is up-to-date'
+            hpack examples/package.yaml | grep -q 'is up-to-date'
+            hpack pirouette-plutusir | grep -q 'is up-to-date'
+            echo "[OK] Package specifications"
+            ormolu --mode check $(find . -name '*.hs') || exit 1
+            echo "[OK] Formatting"
+          '';
+        };
+
         devShells.default = (import ./shell.nix) {
           inherit pkgs;
           inherit haskellPackages;
@@ -16,14 +32,15 @@
 
         ## Same as default without language server &c.
         devShells.ci = pkgs.mkShell {
-          buildInputs = (with haskellPackages; [ ghc cabal-install ])
+          buildInputs = (with haskellPackages; [ ghc cabal-install hpack ])
             ++ (with pkgs; [
               libsodium
-              pkg-config
               secp256k1
+              pkg-config
               zlib
               xz
               postgresql # For pg_config
+              bash
             ]);
           ## Needed by `pirouette-plutusir`
           shellHook = ''
