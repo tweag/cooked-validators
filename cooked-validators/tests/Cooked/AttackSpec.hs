8ef4037531bbfb7656c97cea5dd50e04962e62b7
Carl Hammann <carl.hammann@tweag.io>
2022-12-16 16:35:21 +0100

    Rework addConstraintsAttack into several simpler ones, delete tests

--- cooked-validators/tests/Cooked/AttackSpec.hs
+++ cooked-validators/tests/Cooked/AttackSpec.hs
@@ -1,6 +1,5 @@
 module Cooked.AttackSpec (tests) where
 
--- import qualified Cooked.AttackSpec.AddConstraints as AddConstraints
 import qualified Cooked.Attack.Tweak.CommonSpec as Common
 -- import qualified Cooked.AttackSpec.DatumHijacking as DatumHijacking
 -- import qualified Cooked.AttackSpec.DoubleSat as DoubleSat
@@ -12,8 +11,7 @@ tests :: [TestTree]
 tests =
   [ testGroup
       "Attack DSL"
-      [ -- AddConstraints.tests,
-        Common.tests,
+      [ Common.tests,
         -- DupToken.tests,
         -- DatumHijacking.tests,
         -- DoubleSat.tests,
