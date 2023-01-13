4556ab04da61e47eb8cc079f73cefb976119a3a3
Gabriel Hondet <7418676+gabrielhdt@users.noreply.github.com>
2023-01-13 10:18:23 +0100

    Leaner build system (#187)
    * Use Nixpkg's GHC 8.10.7
    
    Also provide a flake for reproducibility with
    - a default shell suitable for development (hls and stuff)
    - a ci shell accessible with `nix develop .#ci` which doesn't have
      development related dependencies
    
    * CI tweaks
    
    - Put tests that don't require internet (i.e. ormolu and hpack) into
      the flake checks
    - Some sanity on the script 'run-tests' (advised by shellcheck)
    - Update github actions dependencies
    - Update github caching
    
    * Removed shell.nix
    
    Tuned the setup of LD_LIBRARY_PATH
    
    * Activate flake check in CI, fix formatting
    
    * Add dependabot
    
    * Replace cache by artifacts for test output
    
    * Checks exit on error
    
    * Remove envrc
    
    Don't enforce an envrc, let people choose theirs :)
    
    * Bump version of Pirouette
    
    * Add Z3 as a dynamic library
    
    * Also add Z3 to `buildInputs`
    
    * Add dependabot
    
    * Renaming workflows and steps, removed action
    
    The run nix action was not used.
    
    Co-authored-by: Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskell.packages.ghc8107;
+      in {
+        formatter = pkgs.nixfmt;
+
+        ## `cabal test` is not run here because it requires internet
+        ## to fetch the dependencies
+        checks.default = pkgs.stdenv.mkDerivation {
+          name = "ormolu/hpack checks";
+          src = ./.;
+          buildInputs = with pkgs; [ coreutils findutils hpack ormolu ];
+          buildPhase = ''
+            set -o xtrace # See when it fails
+            hpack cooked-validators/package.yaml | grep -q 'is up-to-date' || exit 1
+            hpack examples/package.yaml | grep -q 'is up-to-date' || exit 1
+            hpack pirouette-plutusir/package.yaml | grep -q 'is up-to-date' || exit 1
+            ## NOTE: in case of formatting error, check the versions of
+            ## ormolu and try replacing by ${pkgs.ormolu}/bin/ormolu
+            ## https://discourse.nixos.org/t/nix-shell-buildinputs-ordering-issue/12885/8
+            ormolu --mode check $(find . -name '*.hs') || exit 1
+          '';
+          ## The derivation succeeds if the output is created.
+          installPhase = "mkdir -p $out";
+        };
+
+        devShells = let
+          ## The minimal dependency set to build the project with `cabal`.
+          required = ([ haskellPackages.ghc ]) ++ (with pkgs; [
+            cabal-install
+            libsodium
+            secp256k1
+            pkg-config
+            zlib
+            xz
+            z3
+            postgresql # For pg_config
+          ]);
+          ## Needed by `pirouette-plutusir` and `cooked`
+          LD_LIBRARY_PATH = with pkgs;
+            lib.strings.makeLibraryPath [ libsodium zlib xz z3 ];
+        in {
+          default = pkgs.mkShell {
+            buildInputs = required
+              ++ (with haskellPackages; [ haskell-language-server ])
+              ++ (with pkgs; [ ormolu hpack hlint ]);
+            inherit LD_LIBRARY_PATH;
+          };
+
+          ci = pkgs.mkShell {
+            buildInputs = required;
+            inherit LD_LIBRARY_PATH;
+          };
+        };
+      });
+}
