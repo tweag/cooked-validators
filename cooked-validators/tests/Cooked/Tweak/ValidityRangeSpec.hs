60d54125a06878f78f64682118ddff01c566e658
mmontin <mathieu.montin@enseeiht.fr>
2023-01-25 01:14:27 +0100

    WIP: tests

--- cooked-validators/tests/Cooked/Tweak/ValidityRangeSpec.hs
+++ cooked-validators/tests/Cooked/Tweak/ValidityRangeSpec.hs
@@ -1,12 +1,37 @@
 module Cooked.Tweak.ValidityRangeSpec (tests) where
 
+import Control.Monad (join)
+import Cooked (MonadTweak, runTweak, txSkelTemplate)
+import Cooked.Tweak.ValidityRange (hasFullTimeRangeTweak, isValidDuringTweak, setValidityRangeTweak)
+import Data.Default (def)
+import Data.Either (rights)
+import Data.Function (on)
 import Plutus.V1.Ledger.Interval (interval)
 import Plutus.V2.Ledger.Api (POSIXTime (POSIXTime))
 import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (Assertion, assertBool, testCase)
+
+toTimeRange = interval `on` POSIXTime
+
+getSingleResult = fst . head . rights . flip runTweak txSkelTemplate
+
+checkIsValidDuring :: MonadTweak m => m Assertion
+checkIsValidDuring = do
+  b <- hasFullTimeRangeTweak
+  b1 <- isValidDuringTweak $ toTimeRange 101 1015
+  setValidityRangeTweak $ toTimeRange 101 1015
+  b2 <- isValidDuringTweak $ toTimeRange 110 1000
+  b3 <- isValidDuringTweak $ toTimeRange 80 1015
+  return $
+    assertBool "interval inclusions are wrong" $
+      b && b1 && b2 && not b3
+
+checkAddToValidityRange :: MonadTweak m => m Assertion
+checkAddToValidityRange = do
+  
 
 tests :: TestTree
 tests =
-  let toTimeRange a b = interval (POSIXTime a) (POSIXTime b)
-   in testGroup
-        "Validity range tweaks"
-        []
+  testGroup
+    "Validity range tweaks"
+    [testCase "Validity inclusion" $ getSingleResult checkIsValidDuring]
