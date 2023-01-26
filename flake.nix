88f38fa76c421a0fd74cd07ebbab1efeb605fd77
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-01-26 15:14:19 +0100

    [TEST] Break a Nix file

--- flake.nix
+++ flake.nix
@@ -33,10 +33,8 @@
                         ${pkgs.hpack}/bin/hpack --force "$file"
                     done
                   '';
-                };
-              in "${hpack-dir}/bin/hpack-dir";
-              files =
-                "(\\.l?hs(-boot)?$)|(\\.cabal$)|(^package\\.yaml$)|(/package\\.yaml$)";
+                }; in "${hpack-dir}/bin/hpack-dir";
+              files = "(\\.l?hs(-boot)?$)|(\\.cabal$)|(^package\\.yaml$)|(/package\\.yaml$)";
               pass_filenames = false;
             };
           };
@@ -52,19 +50,15 @@
             secp256k1
             pkg-config
             zlib
-            xz
-            z3
-            postgresql # For pg_config
+            xz z3 postgresql # For pg_config
           ]);
 
           ## Needed by `pirouette-plutusir` and `cooked`
-          LD_LIBRARY_PATH = with pkgs;
-            lib.strings.makeLibraryPath [ libsodium zlib xz z3 ];
+          LD_LIBRARY_PATH = with pkgs; lib.strings.makeLibraryPath [ libsodium zlib xz z3 ];
         in {
           ci = pkgs.mkShell {
             inherit buildInputs;
-            inherit LD_LIBRARY_PATH;
-          };
+            inherit LD_LIBRARY_PATH; };
 
           default = pkgs.mkShell {
             buildInputs = buildInputs
@@ -80,8 +74,6 @@
 
   nixConfig = {
     extra-trusted-substituters = [ "https://tweag-plutus-libs.cachix.org/" ];
-    extra-trusted-public-keys = [
-      "tweag-plutus-libs.cachix.org-1:0BeVJYx8DnUWJWapRDZeLPOOboBUy3UwhvONd5Qm2Xc="
-    ];
+    extra-trusted-public-keys = [ "tweag-plutus-libs.cachix.org-1:0BeVJYx8DnUWJWapRDZeLPOOboBUy3UwhvONd5Qm2Xc=" ];
   };
 }
