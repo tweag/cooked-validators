501bb246e709ddc150299b99490ea09e81c07b66
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-01-31 14:51:41 +0100

    Also reintroduce NOTE from #237

--- flake.nix
+++ flake.nix
@@ -75,6 +75,10 @@
           };
 
           default = pkgs.mkShell {
+            ## NOTE: `pkgs.ormolu` must appear before `hpkgs.haskell-language-server`
+            ## in the `buildInputs`, so as to take precedence. This ensures that the
+            ## version of Ormolu available in the path is that of nixpkgs and not the
+            ## one pinned by HLS.
             buildInputs = buildInputs ++ (with pkgs; [ ormolu hpack hlint ])
               ++ (with hpkgs; [ haskell-language-server ]);
             inherit (pre-commit) shellHook;
