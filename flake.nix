a8aa80ec0abbfadd6bb5345d6d45ed02d0bb0467
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-01-31 14:33:04 +0100

    Get rid of `eachDefaultSystem` (again)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
   outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
-    flake-utils.lib.eachDefaultSystem (system:
+    ## Systemd isn't compatible with Darwin, so we can't use `eachDefaultSystem`
+    ## as long as `systemd` is a dependency.
+    ## TODO: go back to `eachDefaultSystem` once systemd isn't needed anymore.
+    with flake-utils.lib;
+    eachSystem [ system.x86_64-linux system.aarch64-linux ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         hpkgs = pkgs.haskell.packages.ghc8107;
