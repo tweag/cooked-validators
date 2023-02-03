ea52eab066109ba8a48c1455b98e61647284ee8b
Florent C <florentc@users.noreply.github.com>
2023-02-03 16:58:34 +0100

    Requalify imports in `Cooked.Wallet`

--- cooked-validators/src/Cooked/Wallet.hs
+++ cooked-validators/src/Cooked/Wallet.hs
@@ -6,22 +6,22 @@
 
 module Cooked.Wallet where
 
-import qualified Cardano.Api as Api
-import qualified Cardano.Api.Shelley as Api
-import qualified Cardano.Crypto.Wallet as CWCrypto
+import qualified Cardano.Api as Cardano
+import qualified Cardano.Api.Shelley as Cardano
+import qualified Cardano.Crypto.Wallet as Cardano
 import Control.Arrow
 import Data.Default
 import Data.Function (on)
-import qualified Data.Map.Strict as M
-import Data.Maybe
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
 import qualified Ledger as Pl
 import qualified Ledger.Ada as Pl
-import qualified Ledger.CardanoWallet as CW
+import qualified Ledger.CardanoWallet as Pl
 import qualified Ledger.Credential as Pl
-import qualified Ledger.Crypto as Crypto
 import qualified Ledger.Tx.CardanoAPI.Internal as Pl
 import qualified Ledger.Value as Pl
-import qualified Plutus.V2.Ledger.Tx as Pl (OutputDatum (..))
+import qualified Plutus.V2.Ledger.Tx as Pl2 (OutputDatum (..))
 import qualified PlutusTx as Pl
 import Unsafe.Coerce
 
@@ -33,40 +33,40 @@ import Unsafe.Coerce
 -- provide our own wrapper on top of them to make sure that we can easily deal
 -- changes from Plutus.
 
-type Wallet = CW.MockWallet
+type Wallet = Pl.MockWallet
 
-type PrivateKey = CWCrypto.XPrv
+type PrivateKey = Cardano.XPrv
 
 instance Eq Wallet where
-  (==) = (==) `on` CW.mwWalletId
+  (==) = (==) `on` Pl.mwWalletId
 
 instance Ord Wallet where
-  compare = compare `on` CW.mwWalletId
+  compare = compare `on` Pl.mwWalletId
 
 knownWallets :: [Wallet]
-knownWallets = CW.knownMockWallets
+knownWallets = Pl.knownMockWallets
 
 wallet :: Int -> Wallet
 wallet j
   | j > 0 && j <= 10 = let i = j - 1 in knownWallets !! i
-  | otherwise = CW.fromWalletNumber (CW.WalletNumber $ fromIntegral j)
+  | otherwise = Pl.fromWalletNumber (Pl.WalletNumber $ fromIntegral j)
 
 walletPKHashToId :: Pl.PubKeyHash -> Maybe Int
-walletPKHashToId = flip M.lookup walletPKHashToIdMap
+walletPKHashToId = flip Map.lookup walletPKHashToIdMap
   where
-    walletPKHashToIdMap = M.fromList . flip zip [1 ..] . map walletPKHash $ knownWallets
+    walletPKHashToIdMap = Map.fromList . flip zip [1 ..] . map walletPKHash $ knownWallets
 
 walletPK :: Wallet -> Pl.PubKey
-walletPK = Pl.unPaymentPubKey . CW.paymentPubKey
+walletPK = Pl.unPaymentPubKey . Pl.paymentPubKey
 
 walletStakingPK :: Wallet -> Maybe Pl.PubKey
-walletStakingPK = fmap Crypto.toPublicKey . walletStakingSK
+walletStakingPK = fmap Pl.toPublicKey . walletStakingSK
 
 walletPKHash :: Wallet -> Pl.PubKeyHash
 walletPKHash = Pl.pubKeyHash . walletPK
 
 walletStakingPKHash :: Wallet -> Maybe Pl.PubKeyHash
-walletStakingPKHash = fmap Crypto.pubKeyHash . walletStakingPK
+walletStakingPKHash = fmap Pl.pubKeyHash . walletStakingPK
 
 walletAddress :: Wallet -> Pl.Address
 walletAddress w =
@@ -74,28 +74,28 @@ walletAddress w =
     (Pl.PubKeyCredential $ walletPKHash w)
     (Pl.StakingHash . Pl.PubKeyCredential <$> walletStakingPKHash w)
 
-walletSK :: CW.MockWallet -> PrivateKey
-walletSK = Pl.unPaymentPrivateKey . CW.paymentPrivateKey
+walletSK :: Pl.MockWallet -> PrivateKey
+walletSK = Pl.unPaymentPrivateKey . Pl.paymentPrivateKey
 
 -- Massive hack to be able to open a MockPrivateKey; this is needed because
 -- the constructor and accessor to MockPrivateKey are not exported. Hence,
 -- we make an isomorphic datatype, unsafeCoerce to this datatype then extract
 -- whatever we need from it.
-newtype HACK = HACK {please :: CWCrypto.XPrv}
+newtype HACK = HACK {please :: Cardano.XPrv}
 
 -- | Don't use this; its a hack and will be deprecated once we have time
 --  to make a PR into plutus exporting the things we need. If you use this anyway,
 --  make sure that you only apply it to @MockPrivateKey@; the function is polymorphic
 --  because @MockPrivateKey@ is not exported either; having a dedicated function makes
 --  it easy to test that this works: check the @Cooked.WalletSpec@ test module.
-hackUnMockPrivateKey :: a -> CWCrypto.XPrv
+hackUnMockPrivateKey :: a -> Cardano.XPrv
 hackUnMockPrivateKey = please . unsafeCoerce
 
 walletStakingSK :: Wallet -> Maybe PrivateKey
-walletStakingSK = fmap hackUnMockPrivateKey . CW.mwStakeKey
+walletStakingSK = fmap hackUnMockPrivateKey . Pl.mwStakeKey
 
-toPKHMap :: [Wallet] -> M.Map Pl.PubKeyHash Wallet
-toPKHMap ws = M.fromList [(walletPKHash w, w) | w <- ws]
+toPKHMap :: [Wallet] -> Map Pl.PubKeyHash Wallet
+toPKHMap ws = Map.fromList [(walletPKHash w, w) | w <- ws]
 
 -- * Signs a transaction
 
@@ -132,13 +132,13 @@ txAddSignature w = Pl.addSignature' (walletSK w)
 --  and permanent values. (Remember: 1 Ada = 1000000 Lovelace)
 --
 --  Check the corresponding @Default InitialDistribution@ instance for the default value.
-newtype InitialDistribution = InitialDistribution {distribution :: M.Map Wallet [Pl.Value]}
+newtype InitialDistribution = InitialDistribution {distribution :: Map Wallet [Pl.Value]}
   deriving (Eq, Show)
 
 -- | An initial distribution is valid if all utxos being created contain at least
 --  a minimum amount of Ada: 'minAda'.
 validInitialDistribution :: InitialDistribution -> Bool
-validInitialDistribution = all (all hasMinAda . snd) . M.toList . distribution
+validInitialDistribution = all (all hasMinAda . snd) . Map.toList . distribution
   where
     hasMinAda vl = minAda `Pl.leq` vl
 
@@ -152,19 +152,19 @@ ensureHasMinAda val = val <> Pl.toValue missingAda
     missingAda = max 0 $ Pl.minAdaTxOut - Pl.fromValue val
 
 instance Semigroup InitialDistribution where
-  (InitialDistribution i) <> (InitialDistribution j) = InitialDistribution $ M.unionWith (<>) i j
+  (InitialDistribution i) <> (InitialDistribution j) = InitialDistribution $ Map.unionWith (<>) i j
 
 instance Monoid InitialDistribution where
-  mempty = InitialDistribution M.empty
+  mempty = InitialDistribution Map.empty
 
 instance Default InitialDistribution where
-  def = InitialDistribution $ M.fromList $ zip knownWallets (repeat $ replicate 10 defLovelace)
+  def = InitialDistribution $ Map.fromList $ zip knownWallets (repeat $ replicate 10 defLovelace)
     where
       defLovelace = Pl.lovelaceValueOf 100_000_000
 
 -- | Ensures the distribution is valid by adding any missing Ada to all utxos.
 distributionFromList :: [(Wallet, [Pl.Value])] -> InitialDistribution
-distributionFromList = InitialDistribution . M.fromList . fmap (second $ fmap ensureHasMinAda)
+distributionFromList = InitialDistribution . Map.fromList . fmap (second $ fmap ensureHasMinAda)
 
 -- | Extension of the default initial distribution with additional value in
 -- some wallets.
@@ -184,18 +184,18 @@ initialTxFor initDist
     -- initUtxosFor w v = Pl.TxOut $ Api.TxOut addr val Api.TxOutDatumNone Api.ReferenceScriptNone
     initUtxosFor w v = toPlTxOut @() (walletAddress w) v Nothing
 
-    initDist' = M.toList $ distribution initDist
+    initDist' = Map.toList $ distribution initDist
 
     toPlTxOut :: Pl.ToData a => Pl.Address -> Pl.Value -> Maybe a -> Pl.TxOut
     toPlTxOut addr value datum = toPlTxOut' addr value datum'
       where
-        datum' = maybe Pl.NoOutputDatum (Pl.OutputDatumHash . Pl.datumHash . Pl.Datum . Pl.toBuiltinData) datum
+        datum' = maybe Pl2.NoOutputDatum (Pl2.OutputDatumHash . Pl.datumHash . Pl.Datum . Pl.toBuiltinData) datum
 
-    toPlTxOut' :: Pl.Address -> Pl.Value -> Pl.OutputDatum -> Pl.TxOut
+    toPlTxOut' :: Pl.Address -> Pl.Value -> Pl2.OutputDatum -> Pl.TxOut
     toPlTxOut' addr value datum = Pl.TxOut $ toCardanoTxOut' addr value datum
 
-    toCardanoTxOut' :: Pl.Address -> Pl.Value -> Pl.OutputDatum -> Api.TxOut Api.CtxTx Api.BabbageEra
-    toCardanoTxOut' addr value datum = Api.TxOut cAddr cValue cDatum Api.ReferenceScriptNone
+    toCardanoTxOut' :: Pl.Address -> Pl.Value -> Pl2.OutputDatum -> Cardano.TxOut Cardano.CtxTx Cardano.BabbageEra
+    toCardanoTxOut' addr value datum = Cardano.TxOut cAddr cValue cDatum Cardano.ReferenceScriptNone
       where
         fromRight' x = case x of
           Left err -> error $ show err
@@ -204,8 +204,8 @@ initialTxFor initDist
         cValue = fromRight' $ Pl.toCardanoTxOutValue value
         cDatum = fromRight' $ Pl.toCardanoTxOutDatum datum
 
-    theNetworkId :: Api.NetworkId
-    theNetworkId = Api.Testnet $ Api.NetworkMagic 42 -- TODO PORT what's magic?
+    theNetworkId :: Cardano.NetworkId
+    theNetworkId = Cardano.Testnet $ Cardano.NetworkMagic 42 -- TODO PORT what's magic?
 
 valuesForWallet :: InitialDistribution -> Wallet -> [Pl.Value]
-valuesForWallet d w = fromMaybe [] $ w `M.lookup` distribution d
+valuesForWallet d w = fromMaybe [] $ w `Map.lookup` distribution d
