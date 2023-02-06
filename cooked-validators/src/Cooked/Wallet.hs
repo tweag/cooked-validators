367a615715458be3b3fe0a1c8696116e52023693
Florent C <florentc@users.noreply.github.com>
2023-02-06 11:12:06 +0100

    Overhaul comments in Wallet

--- cooked-validators/src/Cooked/Wallet.hs
+++ cooked-validators/src/Cooked/Wallet.hs
@@ -27,7 +27,7 @@ import Unsafe.Coerce
 
 -- $mockchainwallets
 --
--- Because the mock wallets from the plutus-apps changes somewhat often, we will
+-- Because the mock wallets from the plutus-apps changes somewhat often, we
 -- provide our own wrapper on top of them to make sure that we can easily deal
 -- changes from Plutus.
 
@@ -41,46 +41,58 @@ instance Eq Wallet where
 instance Ord Wallet where
   compare = compare `on` Pl.mwWalletId
 
+-- | All the wallets corresponding to known Plutus mock wallets.
 knownWallets :: [Wallet]
 knownWallets = Pl.knownMockWallets
 
+-- | Wallet corresponding to a given wallet number (or wallet ID)
 wallet :: Int -> Wallet
 wallet j
   | j > 0 && j <= 10 = let i = j - 1 in knownWallets !! i
   | otherwise = Pl.fromWalletNumber (Pl.WalletNumber $ fromIntegral j)
 
+-- | Retrieves the id of the known wallet that corresponds to a public key hash, if any.
+--
+-- @walletPKHashToId (walletPKHash (wallet 3)) == Just 3@
 walletPKHashToId :: Pl.PubKeyHash -> Maybe Int
 walletPKHashToId = flip Map.lookup walletPKHashToIdMap
   where
     walletPKHashToIdMap = Map.fromList . flip zip [1 ..] . map walletPKHash $ knownWallets
 
+-- | Retrieves a wallet publik key (PK)
 walletPK :: Wallet -> Pl.PubKey
 walletPK = Pl.unPaymentPubKey . Pl.paymentPubKey
 
+-- | Retrieves a wallet's public staking key (PK), if any
 walletStakingPK :: Wallet -> Maybe Pl.PubKey
 walletStakingPK = fmap Pl.toPublicKey . walletStakingSK
 
+-- | Retrieves a wallet's public key hash
 walletPKHash :: Wallet -> Pl.PubKeyHash
 walletPKHash = Pl.pubKeyHash . walletPK
 
+-- | Retrieves a wallet's public staking key hash, if any
 walletStakingPKHash :: Wallet -> Maybe Pl.PubKeyHash
 walletStakingPKHash = fmap Pl.pubKeyHash . walletStakingPK
 
+-- | Retrieves a wallet's address
 walletAddress :: Wallet -> Pl.Address
 walletAddress w =
   Pl.Address
     (Pl.PubKeyCredential $ walletPKHash w)
     (Pl.StakingHash . Pl.PubKeyCredential <$> walletStakingPKHash w)
 
+-- | Retrieves a wallet private key (secret key SK)
 walletSK :: Pl.MockWallet -> PrivateKey
 walletSK = Pl.unPaymentPrivateKey . Pl.paymentPrivateKey
 
--- Massive hack to be able to open a MockPrivateKey; this is needed because
+-- FIXME Massive hack to be able to open a MockPrivateKey; this is needed because
 -- the constructor and accessor to MockPrivateKey are not exported. Hence,
 -- we make an isomorphic datatype, unsafeCoerce to this datatype then extract
 -- whatever we need from it.
 newtype HACK = HACK Cardano.XPrv
 
+-- | Retrieves a wallet's private staking key (secret key SK), if any
 walletStakingSK :: Wallet -> Maybe PrivateKey
 walletStakingSK = fmap hackUnMockPrivateKey . Pl.mwStakeKey
   where
@@ -95,8 +107,7 @@ walletStakingSK = fmap hackUnMockPrivateKey . Pl.mwStakeKey
 toPKHMap :: [Wallet] -> Map Pl.PubKeyHash Wallet
 toPKHMap ws = Map.fromList [(walletPKHash w, w) | w <- ws]
 
--- * Signs a transaction
-
+-- | Signs a transaction
 txAddSignature :: Wallet -> Pl.Tx -> Pl.Tx
 txAddSignature w = Pl.addSignature' (walletSK w)
 
@@ -153,6 +164,8 @@ distributionFromList = InitialDistribution . Map.fromList
 initialDistribution' :: [(Wallet, [Pl.Value])] -> InitialDistribution
 initialDistribution' = (def <>) . distributionFromList
 
+-- | Bootstraps an initial transaction resulting in a state where wallets
+-- posess UTxOs fitting a given 'InitialDistribution'
 initialTxFor :: InitialDistribution -> Pl.Tx
 initialTxFor initDist =
   mempty
