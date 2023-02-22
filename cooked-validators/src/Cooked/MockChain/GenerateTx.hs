79745a61e2a98bfe294c06d09a1f56f84194ab8d
Carl Hammann <carl.hammann@tweag.io>
2023-02-22 18:25:11 +0100

    ormolu, again

--- cooked-validators/src/Cooked/MockChain/GenerateTx.hs
+++ cooked-validators/src/Cooked/MockChain/GenerateTx.hs
@@ -291,8 +291,8 @@ generateTxBodyContent GenTxParams {..} theParams managedData managedTxOuts manag
 
         witnessMap :: Either GenerateTxError (Map C.PolicyId (C.ScriptWitness C.WitCtxMint C.BabbageEra))
         witnessMap =
-          right mconcat
-            $ mapM
+          right mconcat $
+            mapM
               ( \(policy, redeemer, _tName, _amount) ->
                   Map.singleton
                     <$> left
@@ -300,7 +300,7 @@ generateTxBodyContent GenTxParams {..} theParams managedData managedTxOuts manag
                       (Pl.toCardanoPolicyId (Pl.mintingPolicyHash policy))
                     <*> mkMintWitness policy redeemer
               )
-            $ txSkelMintsToList mints
+              $ txSkelMintsToList mints
 
         mkMintWitness ::
           Pl.Versioned Pl.MintingPolicy ->
