f2543cbb2de504e54b94847766d2570e8154d825
Gabriel Hondet <gabriel.hondet@tweag.io>
2023-02-24 18:47:31 +0100

    Replace postgresql by its library
    libpq.so.5 is needed, but perhaps we can do without postgresql binaries

--- flake.nix
+++ flake.nix
@@ -29,13 +29,12 @@
             pkg-config
             zlib
             xz
-            postgresql # For pg_config
             glibcLocales
           ]);
 
           ## Needed by `pirouette-plutusir` and `cooked`
           LD_LIBRARY_PATH = with pkgs;
-            lib.strings.makeLibraryPath [ libsodium zlib xz ];
+            lib.strings.makeLibraryPath [ libsodium zlib xz postgresql ];
           LANG = "C.UTF-8";
         in {
           ci = pkgs.mkShell {
