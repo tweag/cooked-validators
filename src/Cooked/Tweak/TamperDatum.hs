71bffb893ea63696b5e2f475884ebf98d811047c
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-03-13 14:28:55 +0000

    Untested first draft of a `malformDatumTweak`

--- src/Cooked/Tweak/TamperDatum.hs
+++ src/Cooked/Tweak/TamperDatum.hs
@@ -8,10 +8,13 @@
 module Cooked.Tweak.TamperDatum
   ( tamperDatumTweak,
     TamperDatumLbl (..),
+    malformDatumTweak,
+    MalformDatumLbl (..),
   )
 where
 
 import Control.Monad
+import Cooked.Output
 import Cooked.Pretty.Class
 import Cooked.Skeleton
 import Cooked.Tweak.Common
@@ -52,3 +55,58 @@ tamperDatumTweak change = do
   return beforeModification
 
 data TamperDatumLbl = TamperDatumLbl deriving (Show, Eq, Ord)
+
+-- | FIXME
+malformDatumTweak ::
+  forall a m.
+  ( MonadTweak m,
+    Pl.ToData a,
+    Pl.FromData a,
+    Typeable a
+  ) =>
+  (a -> [Pl.BuiltinData]) ->
+  m ()
+malformDatumTweak change = do
+  outputs <- viewAllTweak (txSkelOutsL % traversed)
+  let modifiedOutputs = map (\output -> output : changeOutput output) outputs
+      modifiedOutputGroups = tail $ allCombinations modifiedOutputs
+  msum $ map (setTweak txSkelOutsL) modifiedOutputGroups
+  where
+    changeOutput :: TxSkelOut -> [TxSkelOut]
+    changeOutput (Pays out) =
+      let datums = changeTxSkelOutDatum $ view outputDatumL out
+       in map
+            ( \datum ->
+                Pays $
+                  ConcreteOutput
+                    (out ^. outputOwnerL)
+                    (out ^. outputStakingCredentialL)
+                    (out ^. outputValueL)
+                    datum
+                    (out ^. outputReferenceScriptL)
+            )
+            datums
+
+    changeTxSkelOutDatum :: TxSkelOutDatum -> [TxSkelOutDatum]
+    changeTxSkelOutDatum TxSkelOutNoDatum = []
+    changeTxSkelOutDatum (TxSkelOutDatum datum) = map TxSkelOutDatum $ changeOnCorrectType datum
+    changeTxSkelOutDatum (TxSkelOutDatumHash datum) = map TxSkelOutDatumHash $ changeOnCorrectType datum
+    changeTxSkelOutDatum (TxSkelOutInlineDatum datum) = map TxSkelOutInlineDatum $ changeOnCorrectType datum
+
+    changeOnCorrectType :: Typeable b => b -> [Pl.BuiltinData]
+    changeOnCorrectType datum = case typeOf datum `eqTypeRep` (typeRep @a) of
+      Just HRefl -> change datum
+      Nothing -> []
+
+data MalformDatumLbl = MalformDatumLbl deriving (Show, Eq, Ord)
+
+-- | FIXME: document
+--
+-- The first element of the result is the list consisting of all the first
+-- elements of the inputs.
+--
+-- @allCombinations [[1,2,3], [4,5], [6]] == [[1,4,6], [1,5,6], [2,4,6], [2,5,6], [3,4,6], [3,5,6]]@
+allCombinations :: [[a]] -> [[a]]
+allCombinations [] = [[]]
+allCombinations [[]] = [] -- included in the next one
+allCombinations (first : rest) = [x : xs | x <- first, xs <- allCombinations rest]
