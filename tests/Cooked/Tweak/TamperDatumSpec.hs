3a0a4c44d99202d7ac0921fb20d52821caafe7e3
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-03-13 18:18:07 +0000

    Cleanup tests

--- tests/Cooked/Tweak/TamperDatumSpec.hs
+++ tests/Cooked/Tweak/TamperDatumSpec.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeApplications #-}
 
---
-
+-- | Tests for 'Cooked.Tweak.TamperDatum'.
 module Cooked.Tweak.TamperDatumSpec where
 
 import Cooked
@@ -12,85 +11,91 @@ import qualified Data.Set as Set
 import qualified Plutus.Script.Utils.Ada as Pl
 import qualified PlutusTx
 import Prettyprinter (viaShow)
-import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@=?))
 
 instance PrettyCooked (Integer, Integer) where
   prettyCookedOpt _ = viaShow
+
+tamperDatumTweakTest =
+  testCase "tamperDatumTweak" $
+    [ Right
+        ( [(52, 53)],
+          txSkelTemplate
+            { txSkelLabel = Set.singleton $ TxLabel TamperDatumLbl,
+              txSkelOuts =
+                [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` (52 :: Integer, 54 :: Integer),
+                  paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
+                  paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer)
+                ]
+            }
+        )
+    ]
+      @=? runTweak
+        ( tamperDatumTweak @(Integer, Integer)
+            (\(x, y) -> if y == 77 then Nothing else Just (x, y + 1))
+        )
+        ( txSkelTemplate
+            { txSkelOuts =
+                [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` (52 :: Integer, 53 :: Integer),
+                  paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
+                  paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer)
+                ]
+            }
+        )
+
+malformDatumTweakTest =
+  testCase "malformDatumTweak" $
+    let txSkelWithDatums1And4 :: (PlutusTx.ToData a, PlutusTx.ToData b) => a -> b -> Either MockChainError ((), TxSkel)
+        txSkelWithDatums1And4 datum1 datum4 =
+          Right
+            ( (),
+              txSkelTemplate
+                { txSkelLabel = Set.singleton $ TxLabel MalformDatumLbl,
+                  txSkelOuts =
+                    [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` PlutusTx.toBuiltinData datum1,
+                      paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
+                      paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer),
+                      paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` PlutusTx.toBuiltinData datum4
+                    ]
+                }
+            )
+     in assertSameSets
+          [ txSkelWithDatums1And4 (52 :: Integer, ()) (84 :: Integer, 85 :: Integer), -- datum1 changed, datum4 untouched
+            txSkelWithDatums1And4 False (84 :: Integer, 85 :: Integer), -- datum1 changed, datum4 untouched
+            txSkelWithDatums1And4 (52 :: Integer, ()) (84 :: Integer, ()), -- datum1 changed, datum4 as well
+            txSkelWithDatums1And4 False False, -- datum1 changed, datum4 as well
+            txSkelWithDatums1And4 (52 :: Integer, ()) False, -- datum1 changed, datum4 as well
+            txSkelWithDatums1And4 False (84 :: Integer, ()), -- datum1 changed, datum4 as well
+            txSkelWithDatums1And4 (52 :: Integer, 53 :: Integer) (84 :: Integer, ()), -- datum1 untouched, datum4 changed
+            txSkelWithDatums1And4 (52 :: Integer, 53 :: Integer) False -- datum1 untouched, datum4 changed
+          ]
+          ( runTweak
+              ( malformDatumTweak @(Integer, Integer)
+                  ( \(x, y) ->
+                      if y == 77
+                        then []
+                        else
+                          [ PlutusTx.toBuiltinData (x, ()),
+                            PlutusTx.toBuiltinData False
+                          ]
+                  )
+              )
+              ( txSkelTemplate
+                  { txSkelOuts =
+                      [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` (52 :: Integer, 53 :: Integer),
+                        paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
+                        paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer),
+                        paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (84 :: Integer, 85 :: Integer)
+                      ]
+                  }
+              )
+          )
 
 tests :: TestTree
 tests =
   testGroup
     "Tamper datum tweaks"
-    [ testCase "tamperDatumTweak" $
-        [ Right
-            ( [(52, 53)],
-              txSkelTemplate
-                { txSkelLabel = Set.singleton $ TxLabel TamperDatumLbl,
-                  txSkelOuts =
-                    [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` (52 :: Integer, 54 :: Integer),
-                      paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
-                      paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer)
-                    ]
-                }
-            )
-        ]
-          @=? runTweak
-            ( tamperDatumTweak @(Integer, Integer)
-                (\(x, y) -> if y == 77 then Nothing else Just (x, y + 1))
-            )
-            ( txSkelTemplate
-                { txSkelOuts =
-                    [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` (52 :: Integer, 53 :: Integer),
-                      paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
-                      paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer)
-                    ]
-                }
-            ),
-      testCase "malformDatumTweak" $
-        let txSkelWithDatums1And4 :: (PlutusTx.ToData a, PlutusTx.ToData b) => a -> b -> Either MockChainError ((), TxSkel)
-            txSkelWithDatums1And4 datum1 datum4 =
-              Right
-                ( (),
-                  txSkelTemplate
-                    { txSkelLabel = Set.singleton $ TxLabel MalformDatumLbl,
-                      txSkelOuts =
-                        [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` PlutusTx.toBuiltinData datum1,
-                          paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
-                          paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer),
-                          paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` PlutusTx.toBuiltinData datum4
-                        ]
-                    }
-                )
-         in assertSameSets
-              [ txSkelWithDatums1And4 (52 :: Integer, ()) (84 :: Integer, 85 :: Integer),
-                txSkelWithDatums1And4 False (84 :: Integer, 85 :: Integer),
-                txSkelWithDatums1And4 (52 :: Integer, ()) (84 :: Integer, ()),
-                txSkelWithDatums1And4 False False,
-                txSkelWithDatums1And4 (52 :: Integer, ()) False,
-                txSkelWithDatums1And4 False (84 :: Integer, ()),
-                txSkelWithDatums1And4 (52 :: Integer, 53 :: Integer) (84 :: Integer, ()),
-                txSkelWithDatums1And4 (52 :: Integer, 53 :: Integer) False
-              ]
-              ( runTweak
-                  ( malformDatumTweak @(Integer, Integer)
-                      ( \(x, y) ->
-                          if y == 77
-                            then []
-                            else
-                              [ PlutusTx.toBuiltinData (x, ()),
-                                PlutusTx.toBuiltinData False
-                              ]
-                      )
-                  )
-                  ( txSkelTemplate
-                      { txSkelOuts =
-                          [ paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 789) `withDatum` (52 :: Integer, 53 :: Integer),
-                            paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 234) `withDatum` (),
-                            paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (76 :: Integer, 77 :: Integer),
-                            paysPK (walletPKHash $ wallet 1) (Pl.lovelaceValueOf 567) `withDatum` (84 :: Integer, 85 :: Integer)
-                          ]
-                      }
-                  )
-              )
+    [ tamperDatumTweakTest,
+      malformDatumTweakTest
     ]
