d978953a26bc12f4a980255e3c0443c9e16d893d
mmontin <mathieu.montin@tweag.io>
2024-03-18 15:52:32 +0100

    improving tests

--- tests/Cooked/InitialDistributionSpec.hs
+++ tests/Cooked/InitialDistributionSpec.hs
@@ -7,56 +7,63 @@ import Control.Monad
 import Cooked
 import Data.Default
 import qualified Data.Map as Map
+import Data.Maybe (catMaybes)
 import qualified Plutus.Script.Utils.V2.Typed.Scripts.Validators as Pl
 import Test.Tasty
 import Test.Tasty.HUnit
 
--- | An initial distribution where wallet 1 owns a UTxO with a datum
--- of type Int and value 10
-initialDistributionWithDatum =
-  InitialDistribution
-    [withInlineDatum @Integer (paysPK (walletPKHash $ wallet 1) (ada 2)) 10]
+alice = wallet 1
 
--- | An initial distribution where wallet 1 owns a UTxO with a
+alicePKH = walletPKHash alice
+
+bob = wallet 2
+
+bobPKH = walletPKHash bob
+
+-- | An initial distribution where alice owns a UTxO with a datum
+-- of type Int and value 10 for each datum kind
+initialDistributionWithDatum =
+  InitialDistribution $
+    (\f -> f (paysPK alicePKH (ada 2)) 10)
+      <$> [withDatum @Integer, withInlineDatum, withDatumHash]
+
+-- | An initial distribution where alice owns a UTxO with a
 -- reference script corresponding to the always succeed validators
--- and wallet 2 owns 2 UTxOs with 100 ada
+-- and bob owns 2 UTxOs with 100 ada
 initialDistributionWithReferenceScript =
   InitialDistribution $
-    (paysPK (walletPKHash $ wallet 1) (ada 2) `withReferenceScript` alwaysTrueValidator @MockContract)
-      : replicate 2 (paysPK (walletPKHash $ wallet 2) (ada 100))
+    (paysPK alicePKH (ada 2) `withReferenceScript` alwaysTrueValidator @MockContract)
+      : replicate 2 (paysPK bobPKH (ada 100))
 
-getValueFromInitialDatum :: (MonadBlockChain m) => m Integer
+getValueFromInitialDatum :: (MonadBlockChain m) => m [Integer]
 getValueFromInitialDatum = do
-  [(txOutRef, _)] <- runUtxoSearch $ utxosAtSearch $ walletAddress $ wallet 1
-  Just datum <- typedDatumFromTxOutRef @Integer txOutRef
-  return datum
-
-printAndRun initDist trace = printCooked $ fst <$> interpretAndRunWith (runMockChainTFrom initDist) trace
+  aliceUtxos <- runUtxoSearch $ utxosAtSearch $ walletAddress alice
+  catMaybes <$> mapM (typedDatumFromTxOutRef @Integer . fst) aliceUtxos
 
 spendReferenceAlwaysTrueValidator :: (MonadBlockChain m) => m ()
 spendReferenceAlwaysTrueValidator = do
-  [(referenceScriptTxOutRef, _)] <- runUtxoSearch $ utxosAtSearch $ walletAddress $ wallet 1
+  [(referenceScriptTxOutRef, _)] <- runUtxoSearch $ utxosAtSearch $ walletAddress alice
   tx <-
     validateTxSkel $
       txSkelTemplate
         { txSkelOuts = [paysScript (alwaysTrueValidator @MockContract) () (ada 2)],
-          txSkelSigners = [wallet 2]
+          txSkelSigners = [bob]
         }
   let (scriptTxOutRef, _) : _ = utxosFromCardanoTx tx
   void $
     validateTxSkel $
       txSkelTemplate
-        { txSkelOuts = [paysPK (walletPKHash $ wallet 1) (ada 2)],
+        { txSkelOuts = [paysPK alicePKH (ada 2)],
           txSkelIns = Map.singleton scriptTxOutRef (TxSkelRedeemerForReferencedScript referenceScriptTxOutRef ()),
-          txSkelSigners = [wallet 2]
+          txSkelSigners = [bob]
         }
 
 tests :: TestTree
 tests =
   testGroup
     "initial distributions"
-    [ testCase "Reading a datum placed in the initial distribution" $
-        testSucceedsFrom' def (\n _ -> testBool $ n == 10) initialDistributionWithDatum getValueFromInitialDatum,
+    [ testCase "Reading datums placed in the initial distribution, inlined, hashed or vanilla" $
+        testSucceedsFrom' def (\results _ -> testBool $ results == [10, 10, 10]) initialDistributionWithDatum getValueFromInitialDatum,
       testCase "Spending a script placed as a reference script in the initial distribution" $
         testSucceedsFrom def initialDistributionWithReferenceScript spendReferenceAlwaysTrueValidator
     ]
