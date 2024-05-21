3d6a40c4bb8d61f502fe753ae71c2c5292678190
mmontin <mathieu.montin@tweag.io>
2024-05-21 17:27:43 +0200

    Classes to Wrappers and more comments

--- src/Cooked/Wrappers.hs
+++ src/Cooked/Wrappers.hs
@@ -1,4 +1,12 @@
-module Cooked.Classes where
+-- | This module provides various wrapper classes to identify which
+-- objects possess which properties. This is meant to greatly simplify
+-- the use of the rest of cooked-validators. For instance, when paying
+-- to a wallet, all we need is its address. Retrieving it will
+-- walletAddress is inconvenient as we know a wallet possess an
+-- address. By creating a type class `HasAddress` we overcome this
+-- limitation and allow payments directly both to addresses or
+-- wallets.
+module Cooked.Wrappers where
 
 import Cooked.Wallet
 import Plutus.Script.Utils.Ada qualified as Script
@@ -6,6 +14,7 @@ import Plutus.Script.Utils.Scripts qualified as Script
 import Plutus.Script.Utils.Typed qualified as Script
 import PlutusLedgerApi.V3 qualified as Api
 
+-- | Objects from which and address can be extracted
 class HasAddress a where
   toAddress :: a -> Api.Address
 
@@ -18,6 +27,7 @@ instance HasAddress Api.Address where
 instance HasAddress (Script.TypedValidator a) where
   toAddress = Script.validatorAddress
 
+-- | Objects from which a public key hash can be extracted
 class HasPubKeyHash a where
   toPubKeyHash :: a -> Api.PubKeyHash
 
@@ -27,6 +37,7 @@ instance HasPubKeyHash Api.PubKeyHash where
 instance HasPubKeyHash Wallet where
   toPubKeyHash = walletPKHash
 
+-- | Objects from which a credential can be extracted
 class ToCredential a where
   toCredential :: a -> Api.Credential
 
@@ -34,11 +45,12 @@ instance ToCredential Api.Credential where
   toCredential = id
 
 instance ToCredential (Script.TypedValidator a) where
-  toCredential = Api.ScriptCredential . toScriptHash . Script.tvValidatorHash
+  toCredential = Api.ScriptCredential . toScriptHash
 
 instance ToCredential Api.PubKeyHash where
   toCredential = Api.PubKeyCredential
 
+-- | Objects from which an output datum can be extracted
 class ToOutputDatum a where
   toOutputDatum :: a -> Api.OutputDatum
 
@@ -57,6 +69,7 @@ instance ToOutputDatum Api.DatumHash where
 instance ToOutputDatum Api.BuiltinData where
   toOutputDatum = toOutputDatum . Api.Datum
 
+-- | Objects from which a value can be extracted
 class ToValue a where
   toValue :: a -> Api.Value
 
@@ -66,6 +79,7 @@ instance ToValue Api.Value where
 instance ToValue Script.Ada where
   toValue = Script.toValue
 
+-- | Objects from which a versioned script can be extracted
 class ToScript a where
   toScript :: a -> Script.Versioned Script.Script
 
@@ -78,6 +92,7 @@ instance ToScript (Script.Versioned Script.Validator) where
 instance ToScript (Script.TypedValidator a) where
   toScript = toScript . Script.vValidatorScript
 
+-- | Objects from which a script hash can be extracted
 class ToScriptHash a where
   toScriptHash :: a -> Api.ScriptHash
 
