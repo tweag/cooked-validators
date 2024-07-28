8764a1aaf7eedc0adc49151cad07397850da6e36
mmontin <mathieu.montin@tweag.io>
2024-07-28 11:53:53 +0200

    typo

--- src/Cooked/MockChain/Balancing.hs
+++ src/Cooked/MockChain/Balancing.hs
@@ -94,7 +94,7 @@ balanceTxSkel skelUnbal@TxSkel {..} = do
               -- warning if any was actually discarded.
               >>= filterAndWarn (isJust . isPKOutput . snd) "They belong to scripts."
           -- We filter the candidate utxos by removing those already present in the
-          -- skeleton, throwing a warning if any was actually discarder
+          -- skeleton, throwing a warning if any was actually discarded
           >>= filterAndWarn ((`notElem` txSkelKnownTxOutRefs skelUnbal) . fst) "They are already used in the skeleton."
 
       case txOptFeePolicy txSkelOpts of
