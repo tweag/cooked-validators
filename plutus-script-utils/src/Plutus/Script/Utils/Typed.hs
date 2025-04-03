48b926aea00567769bbc9a50c9540afe0b72a8d4
mmontin <mathieu.montin@moduscreate.com>
2025-04-03 16:32:40 +0200

    getting rid of IsScriptContext

--- plutus-script-utils/src/Plutus/Script/Utils/Typed.hs
+++ plutus-script-utils/src/Plutus/Script/Utils/Typed.hs
@@ -12,9 +12,9 @@ module Plutus.Script.Utils.Typed
     forwardingMintingPolicyHash,
     generalise,
     Any,
-    IsScriptContext (..),
-    ScriptContextV1,
-    ScriptContextV2,
+    mkUntypedValidator,
+    mkUntypedStakeValidator,
+    mkUntypedMintingPolicy,
   )
 where
 
@@ -44,7 +44,6 @@ import Plutus.Script.Utils.Scripts
     getValidator,
   )
 import PlutusLedgerApi.V1 qualified as PV1
-import PlutusLedgerApi.V2 qualified as PV2
 import PlutusTx.Prelude (BuiltinData, BuiltinString, BuiltinUnit, check, trace)
 import PlutusTx.Prelude qualified as PlutusTx
 
@@ -149,178 +148,158 @@ vForwardingMintingPolicy = tvForwardingMPS
 forwardingMintingPolicyHash :: TypedValidator a -> MintingPolicyHash
 forwardingMintingPolicyHash = tvForwardingMPSHash
 
-{-# INLINEABLE tracedUnsafeFrom #-}
-tracedUnsafeFrom :: (PV1.FromData a) => BuiltinString -> BuiltinData -> Maybe a
-tracedUnsafeFrom label builtinData = do
+{-# INLINEABLE tracedSafeFrom #-}
+tracedSafeFrom :: (PV1.FromData a) => BuiltinString -> BuiltinData -> Maybe a
+tracedSafeFrom label builtinData = do
   dat <- PV1.fromBuiltinData builtinData
   -- We trace after the decoding is actually successful
   return $ trace label dat
 
-class (PV1.FromData sc) => IsScriptContext sc where
-  {-# INLINEABLE mkUntypedValidator #-}
+-- | Converts a custom datum and redeemer from a validator function to an
+-- untyped validator function. See Note [Scripts returning Bool].
+--
+-- Here's an example of how this function can be used:
+--
+-- @
+--   import PlutusTx qualified
+--   import PlutusLedgerApi.V2.Scripts qualified as Plutus
+--   import Plutus.Script.Utils.V2.Scripts (mkUntypedValidator)
+--
+--   newtype MyCustomDatum = MyCustomDatum Integer
+--   PlutusTx.unstableMakeIsData ''MyCustomDatum
+--   newtype MyCustomRedeemer = MyCustomRedeemer Integer
+--   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
+--
+--   mkValidator :: MyCustomDatum -> MyCustomRedeemer -> Plutus.ScriptContext -> Bool
+--   mkValidator _ _ _ = True
+--
+--   validator :: Plutus.Validator
+--   validator = Plutus.mkValidatorScript
+--       $$(PlutusTx.compile [|| wrap ||])
+--    where
+--       wrap = mkUntypedValidator mkValidator
+-- @
+--
+-- Here's an example using a parameterized validator:
+--
+-- @
+--   import PlutusTx qualified
+--   import PlutusLedgerApi.V2.Scripts qualified as Plutus
+--   import Plutus.Script.Utils.V2.Scripts (mkUntypedValidator)
+--
+--   newtype MyCustomDatum = MyCustomDatum Integer
+--   PlutusTx.unstableMakeIsData ''MyCustomDatum
+--   newtype MyCustomRedeemer = MyCustomRedeemer Integer
+--   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
+--
+--   mkValidator :: Int -> MyCustomDatum -> MyCustomRedeemer -> Plutus.ScriptContext -> Bool
+--   mkValidator _ _ _ _ = True
+--
+--   validator :: Int -> Plutus.Validator
+--   validator i = Plutus.mkValidatorScript
+--       $$(PlutusTx.compile [|| wrap . mkValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode i
+--    where
+--       wrap = mkUntypedValidator
+-- @
+--
+-- For debugging purpose, it may be of interest to know that in the default
+-- implementation, the parameters are decoded in the order they appear (data,
+-- redeemer and then script context). A log trace is generated after each
+-- successfully decoded parameter.  Thus, if a parameter can't be decoded, the
+-- culprit is the first parameter in the list that doesn't appear as
+-- successfully decoded in the log trace.
+{-# INLINEABLE mkUntypedValidator #-}
+mkUntypedValidator ::
+  (PV1.FromData datum, PV1.FromData redeemer, PV1.FromData scriptContext) =>
+  (datum -> redeemer -> scriptContext -> Bool) ->
+  UntypedValidator
+mkUntypedValidator f d r sc =
+  check $
+    PlutusTx.fromMaybe
+      False
+      ( do
+          dat <- tracedSafeFrom "Datum decoded successfully" d
+          red <- tracedSafeFrom "Redeemer decoded successfully" r
+          ctx <- tracedSafeFrom "Script context decoded successfully" sc
+          return $ f dat red ctx
+      )
 
-  -- | Converts a custom datum and redeemer from a validator function to an
-  -- untyped validator function. See Note [Scripts returning Bool].
-  --
-  -- Here's an example of how this function can be used:
-  --
-  -- @
-  --   import PlutusTx qualified
-  --   import PlutusLedgerApi.V2.Scripts qualified as Plutus
-  --   import Plutus.Script.Utils.V2.Scripts (mkUntypedValidator)
-  --
-  --   newtype MyCustomDatum = MyCustomDatum Integer
-  --   PlutusTx.unstableMakeIsData ''MyCustomDatum
-  --   newtype MyCustomRedeemer = MyCustomRedeemer Integer
-  --   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
-  --
-  --   mkValidator :: MyCustomDatum -> MyCustomRedeemer -> Plutus.ScriptContext -> Bool
-  --   mkValidator _ _ _ = True
-  --
-  --   validator :: Plutus.Validator
-  --   validator = Plutus.mkValidatorScript
-  --       $$(PlutusTx.compile [|| wrap ||])
-  --    where
-  --       wrap = mkUntypedValidator mkValidator
-  -- @
-  --
-  -- Here's an example using a parameterized validator:
-  --
-  -- @
-  --   import PlutusTx qualified
-  --   import PlutusLedgerApi.V2.Scripts qualified as Plutus
-  --   import Plutus.Script.Utils.V2.Scripts (mkUntypedValidator)
-  --
-  --   newtype MyCustomDatum = MyCustomDatum Integer
-  --   PlutusTx.unstableMakeIsData ''MyCustomDatum
-  --   newtype MyCustomRedeemer = MyCustomRedeemer Integer
-  --   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
-  --
-  --   mkValidator :: Int -> MyCustomDatum -> MyCustomRedeemer -> Plutus.ScriptContext -> Bool
-  --   mkValidator _ _ _ _ = True
-  --
-  --   validator :: Int -> Plutus.Validator
-  --   validator i = Plutus.mkValidatorScript
-  --       $$(PlutusTx.compile [|| wrap . mkValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode i
-  --    where
-  --       wrap = mkUntypedValidator
-  -- @
-  --
-  -- For debugging purpose, it may be of interest to know that in the default
-  -- implementation, the parameters are decoded in the order they appear (data,
-  -- redeemer and then script context). A log trace is generated after each
-  -- successfully decoded parameter.  Thus, if a parameter can't be decoded, the
-  -- culprit is the first parameter in the list that doesn't appear as
-  -- successfully decoded in the log trace.
-  mkUntypedValidator ::
-    (PV1.FromData d, PV1.FromData r) =>
-    (d -> r -> sc -> Bool) ->
-    UntypedValidator
-  mkUntypedValidator f d r p =
-    check $
-      PlutusTx.fromMaybe
-        False
-        ( do
-            dat <- tracedUnsafeFrom "Datum decoded successfully" d
-            red <- tracedUnsafeFrom "Redeemer decoded successfully" r
-            sc <- tracedUnsafeFrom "Script context decoded successfully" p
-            return $ f dat red sc
-        )
+-- | Converts a custom redeemer from a stake validator function to an
+-- untyped stake validator function.
+--
+-- Here's an example of how this function can be used:
+--
+-- @
+--   import PlutusTx qualified
+--   import PlutusLedgerApi.V1.Scripts qualified as Plutus
+--   import Plutus.Script.Utils.V1.Scripts (mkUntypedStakeValidator)
+--
+--   newtype MyCustomRedeemer = MyCustomRedeemer Integer
+--   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
+--
+--   mkStakeValidator :: MyCustomRedeemer -> ScriptContext -> Bool
+--   mkStakeValidator _ _ = True
+--
+--   validator :: Plutus.Validator
+--   validator = Plutus.mkStakeValidatorScript
+--       $$(PlutusTx.compile [|| wrap ||])
+--    where
+--       wrap = mkUntypedStakeValidator mkStakeValidator
+-- @
+--
+-- For debugging purpose, it may be of interest to know that in the default
+-- implementation, the parameters are decoded in the order they appear
+-- (redeemer and then script context). A log trace is generated after each
+-- successfully decoded parameter.  Thus, if a parameter can't be decoded, the
+-- culprit is the first parameter in the list that doesn't appear as
+-- successfully decoded in the log trace.
+{-# INLINEABLE mkUntypedStakeValidator #-}
+mkUntypedStakeValidator ::
+  (PV1.FromData redeemer, PV1.FromData scriptContext) =>
+  (redeemer -> scriptContext -> Bool) ->
+  UntypedStakeValidator
+mkUntypedStakeValidator f r sc =
+  check $
+    PlutusTx.fromMaybe
+      False
+      ( do
+          red <- tracedSafeFrom "Redeemer decoded successfully" r
+          ctx <- tracedSafeFrom "Script context decoded successfully" sc
+          return $ f red ctx
+      )
 
-  {-# INLINEABLE mkUntypedStakeValidator #-}
-
-  -- | Converts a custom redeemer from a stake validator function to an
-  -- untyped stake validator function. See Note [Scripts returning Bool].
-  --
-  -- Here's an example of how this function can be used:
-  --
-  -- @
-  --   import PlutusTx qualified
-  --   import PlutusLedgerApi.V1.Scripts qualified as Plutus
-  --   import Plutus.Script.Utils.V1.Scripts (mkUntypedStakeValidator)
-  --
-  --   newtype MyCustomRedeemer = MyCustomRedeemer Integer
-  --   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
-  --
-  --   mkStakeValidator :: MyCustomRedeemer -> ScriptContext -> Bool
-  --   mkStakeValidator _ _ = True
-  --
-  --   validator :: Plutus.Validator
-  --   validator = Plutus.mkStakeValidatorScript
-  --       $$(PlutusTx.compile [|| wrap ||])
-  --    where
-  --       wrap = mkUntypedStakeValidator mkStakeValidator
-  -- @
-  --
-  -- For debugging purpose, it may be of interest to know that in the default
-  -- implementation, the parameters are decoded in the order they appear
-  -- (redeemer and then script context). A log trace is generated after each
-  -- successfully decoded parameter.  Thus, if a parameter can't be decoded, the
-  -- culprit is the first parameter in the list that doesn't appear as
-  -- successfully decoded in the log trace.
-  mkUntypedStakeValidator ::
-    (PV1.FromData r) =>
-    (r -> sc -> Bool) ->
-    UntypedStakeValidator
-  mkUntypedStakeValidator f r p =
-    check $
-      PlutusTx.fromMaybe
-        False
-        ( do
-            red <- tracedUnsafeFrom "Redeemer decoded successfully" r
-            sc <- tracedUnsafeFrom "Script context decoded successfully" p
-            return $ f red sc
-        )
-
-  {-# INLINEABLE mkUntypedMintingPolicy #-}
-
-  -- | Converts a custom redeemer from a minting policy function to an
-  -- untyped minting policy function. See Note [Scripts returning Bool].
-  --
-  -- Here's an example of how this function can be used:
-  --
-  -- @
-  --   import PlutusTx qualified
-  --   import PlutusLedgerApi.V1.Scripts qualified as Plutus
-  --   import Plutus.Script.Utils.V1.Scripts (mkUntypedMintingPolicy)
-  --
-  --   newtype MyCustomRedeemer = MyCustomRedeemer Integer
-  --   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
-  --
-  --   mkMintingPolicy :: MyCustomRedeemer -> ScriptContext -> Bool
-  --   mkMintingPolicy _ _ = True
-  --
-  --   validator :: Plutus.Validator
-  --   validator = Plutus.mkMintingPolicyScript
-  --       $$(PlutusTx.compile [|| wrap ||])
-  --    where
-  --       wrap = mkUntypedMintingPolicy mkMintingPolicy
-  -- @
-  --
-  -- For debugging purpose, it may be of interest to know that in the default
-  -- implementation, the parameters are decoded in the order they appear
-  -- (redeemer and then script context). A log trace is generated after each
-  -- successfully decoded parameter.  Thus, if a parameter can't be decoded, the
-  -- culprit is the first parameter in the list that doesn't appear as
-  -- successfully decoded in the log trace.
-  mkUntypedMintingPolicy ::
-    (PV1.FromData r) =>
-    (r -> sc -> Bool) ->
-    UntypedMintingPolicy
-  mkUntypedMintingPolicy f r p =
-    check $
-      PlutusTx.fromMaybe
-        False
-        ( do
-            red <- tracedUnsafeFrom "Redeemer decoded successfully" r
-            sc <- tracedUnsafeFrom "Script context decoded successfully" p
-            return $ f red sc
-        )
-
-type ScriptContextV1 = PV1.ScriptContext
-
-type ScriptContextV2 = PV2.ScriptContext
-
-instance IsScriptContext PV1.ScriptContext
-
-instance IsScriptContext PV2.ScriptContext
+-- | Converts a custom redeemer from a minting policy function to an
+-- untyped minting policy function.
+--
+-- Here's an example of how this function can be used:
+--
+-- @
+--   import PlutusTx qualified
+--   import PlutusLedgerApi.V1.Scripts qualified as Plutus
+--   import Plutus.Script.Utils.V1.Scripts (mkUntypedMintingPolicy)
+--
+--   newtype MyCustomRedeemer = MyCustomRedeemer Integer
+--   PlutusTx.unstableMakeIsData ''MyCustomRedeemer
+--
+--   mkMintingPolicy :: MyCustomRedeemer -> ScriptContext -> Bool
+--   mkMintingPolicy _ _ = True
+--
+--   validator :: Plutus.Validator
+--   validator = Plutus.mkMintingPolicyScript
+--       $$(PlutusTx.compile [|| wrap ||])
+--    where
+--       wrap = mkUntypedMintingPolicy mkMintingPolicy
+-- @
+--
+-- For debugging purpose, it may be of interest to know that in the default
+-- implementation, the parameters are decoded in the order they appear
+-- (redeemer and then script context). A log trace is generated after each
+-- successfully decoded parameter.  Thus, if a parameter can't be decoded, the
+-- culprit is the first parameter in the list that doesn't appear as
+-- successfully decoded in the log trace.
+{-# INLINEABLE mkUntypedMintingPolicy #-}
+mkUntypedMintingPolicy ::
+  (PV1.FromData redeemer, PV1.FromData scriptContext) =>
+  (redeemer -> scriptContext -> Bool) ->
+  UntypedMintingPolicy
+mkUntypedMintingPolicy = mkUntypedStakeValidator
