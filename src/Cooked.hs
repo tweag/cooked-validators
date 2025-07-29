988d55b90958e973b19a96621454224f6c1a3174
mmontin <o.montin@gmail.com>
2025-07-29 17:10:59 +0200

    fixing exports

--- src/Cooked.hs
+++ src/Cooked.hs
@@ -1,10 +1,16 @@
 -- | Re-exports the entirety of the library, which is always eventually necessary
 --  when writing large test-suites.
-module Cooked (module X) where
+module Cooked
+  ( module X,
+    Ltl (..),
+    MonadModal (..),
+    ltlDelay,
+  )
+where
 
 import Cooked.Attack as X
 import Cooked.InitialDistribution as X
-import Cooked.Ltl as X
+import Cooked.Ltl (Ltl (..), MonadModal (..), ltlDelay)
 import Cooked.MockChain as X
 import Cooked.Pretty as X
 import Cooked.ShowBS as X
