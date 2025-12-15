33f996375eb030f4082fded559937afa8f03daba
mmontin <o.montin@gmail.com>
2025-12-16 00:21:51 +0100

    grouping ltl tests

--- tests/Spec/Ltl.hs
+++ tests/Spec/Ltl.hs
@@ -192,5 +192,20 @@ tests =
                       >> emitInteger 45
               )
               [[42 + 1, 43 + 1 + 2, 44 * 3 + 1 + 2, 45 + 1]]
+        ],
+      testGroup
+        "LTL Combinators"
+        [ testCase "anyOf [1] - no LtlOr" $ anyOf @Int [1] @?= LtlAtom 1,
+          testCase "anyOf [1,2] - single LtlOr" $ anyOf @Int [1, 2] @?= LtlOr (LtlAtom 1) (LtlAtom 2),
+          testCase "anyOf [1,2,3] - right biased LtlOr tree" $ anyOf @Int [1, 2, 3] @?= LtlOr (LtlAtom 1) (LtlOr (LtlAtom 2) (LtlAtom 3)),
+          testCase "allOf [1] - no LtlAnd" $ allOf @Int [1] @?= LtlAtom 1,
+          testCase "allOf [1,2] - single LtlAnd" $ allOf @Int [1, 2] @?= LtlAnd (LtlAtom 1) (LtlAtom 2),
+          testCase "allOf [1,2,3] - right biased LtlAnd tree" $ allOf @Int [1, 2, 3] @?= LtlAnd (LtlAtom 1) (LtlAnd (LtlAtom 2) (LtlAtom 3)),
+          testCase "delay' -1" $ delay' @Int (-1) LtlTruth @?= LtlTruth,
+          testCase "delay' 0" $ delay' @Int 0 LtlTruth @?= LtlTruth,
+          testCase "delay' 1" $ delay' @Int 1 LtlTruth @?= LtlNext LtlTruth,
+          testCase "delay' 2" $ delay' @Int 2 LtlTruth @?= LtlNext (LtlNext LtlTruth),
+          testCase "eventually" $ eventually @Int 1 @?= LtlUntil LtlTruth (LtlAtom 1),
+          testCase "always" $ always @Int 1 @?= LtlRelease LtlFalsity (LtlAtom 1)
         ]
     ]
