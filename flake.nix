c4de79e48e32d69f29dafdaf457faae6392d4dab
mmontin <mathieu.montin@moduscreate.com>
2026-01-25 20:38:08 +0100

    migrating temporarily to the haskell-update branch from nixpkgs to get cabal-install 3.16.1.0

--- flake.nix
+++ flake.nix
@@ -1,12 +1,18 @@
 {
-  inputs.nixpkgs.url =
-    "github:NixOS/nixpkgs/28ace32529a63842e4f8103e4f9b24960cf6c23a";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/haskell-updates";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   inputs.pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pre-commit-hooks,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         hpkgs = pkgs.haskell.packages.ghc96;
@@ -16,16 +22,15 @@
         ## is due to a bug where older processors (>= 10 years)
         ## would not be supported. This should not change anything
         ## on newer machines. This could be revised in the future.
-        blst-portable = pkgs.blst.overrideAttrs (_: _: {
-          buildPhase = ''
-            runHook preBuild
-            ./build.sh -shared -D__BLST_PORTABLE__ ${
-              pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isWindows
-              "flavour=mingw64"
-            }
-            runHook postBuild
-          '';
-        });
+        blst-portable = pkgs.blst.overrideAttrs (
+          _: _: {
+            buildPhase = ''
+              runHook preBuild
+              ./build.sh -shared -D__BLST_PORTABLE__ ${pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isWindows "flavour=mingw64"}
+              runHook postBuild
+            '';
+          }
+        );
 
         pre-commit = pre-commit-hooks.lib.${system}.run {
           src = ./.;
@@ -46,75 +51,79 @@
             ## for more information.
           };
         };
-      in {
+      in
+      {
         formatter = pkgs.nixfmt;
 
-        devShells = let
-          ## The minimal dependency set to build the project with `cabal`.
-          buildInputs = [
-            blst-portable
-            pkgs.pkg-config
-            pkgs.glibcLocales
-            pkgs.zlib
-            pkgs.libsodium
-            pkgs.secp256k1
-            pkgs.lmdb
-            hpkgs.ghc
-            hpkgs.cabal-install
-          ];
-
-          ## Folders in which to find ".so" files
-          LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath [
-            pkgs.xz
-            pkgs.zlib
-            pkgs.lmdb
-            pkgs.openssl_3_4
-            pkgs.postgresql # For cardano-node-emulator
-            pkgs.openldap # For freer-extras‽
-            pkgs.libsodium
-            pkgs.secp256k1
-            pkgs.lmdb
-            blst-portable
-          ];
-
-          LANG = "C.UTF-8";
-
-        in {
-          ci = pkgs.mkShell {
-            inherit buildInputs;
-            inherit LD_LIBRARY_PATH;
-            inherit LANG;
-          };
-
-          default = pkgs.mkShell {
-            buildInputs = buildInputs ++ [
-              pkgs.hpack
-              pkgs.hlint
-              hpkgs.ormolu
-              hpkgs.haskell-language-server
+        devShells =
+          let
+            ## The minimal dependency set to build the project with `cabal`.
+            buildInputs = [
+              blst-portable
+              pkgs.pkg-config
+              pkgs.glibcLocales
+              pkgs.zlib
+              pkgs.libsodium
+              pkgs.secp256k1
+              pkgs.lmdb
+              hpkgs.ghc
+              hpkgs.cabal-install
             ];
 
-            inherit LD_LIBRARY_PATH;
-            inherit LANG;
+            ## Folders in which to find ".so" files
+            LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath [
+              pkgs.xz
+              pkgs.zlib
+              pkgs.lmdb
+              pkgs.openssl_3_6
+              pkgs.postgresql # For cardano-node-emulator
+              pkgs.openldap # For freer-extras‽
+              pkgs.libsodium
+              pkgs.secp256k1
+              pkgs.lmdb
+              blst-portable
+            ];
 
-            # In addition to the pre-commit hooks, this redefines a cabal
-            # command that gets rid of annoying "Writing: .....*.html" output
-            # when running cabal test.
-            shellHook = pre-commit.shellHook + ''
-              function cabal() {
-                    if [ "$1" != "test" ]; then
-                      command cabal "$@"
-                    else
-                      command cabal --test-option=--color=always "$@" | grep -vE --color=never "^Writing:.*html$"
-                    fi
-              }
-              export -f cabal
-            '';
+            LANG = "C.UTF-8";
+
+          in
+          {
+            ci = pkgs.mkShell {
+              inherit buildInputs;
+              inherit LD_LIBRARY_PATH;
+              inherit LANG;
+            };
+
+            default = pkgs.mkShell {
+              buildInputs = buildInputs ++ [
+                pkgs.hpack
+                pkgs.hlint
+                hpkgs.ormolu
+                hpkgs.haskell-language-server
+              ];
+
+              inherit LD_LIBRARY_PATH;
+              inherit LANG;
+
+              # In addition to the pre-commit hooks, this redefines a cabal
+              # command that gets rid of annoying "Writing: .....*.html" output
+              # when running cabal test.
+              shellHook = pre-commit.shellHook + ''
+                function cabal() {
+                      if [ "$1" != "test" ]; then
+                        command cabal "$@"
+                      else
+                        command cabal --test-option=--color=always "$@" | grep -vE --color=never "^Writing:.*html$"
+                      fi
+                }
+                export -f cabal
+              '';
+            };
           };
-        };
 
         checks = { inherit pre-commit; };
-      });
+      }
+    );
 
   nixConfig = {
     extra-trusted-substituters = [
