34f04769e7a6ac9e2d2988d2bd05ee8c8d313093
mmontin <mathieu.montin@moduscreate.com>
2026-08-14 09:58:34 +0200

    using searches

--- src/Cooked/TestNode.hs
+++ src/Cooked/TestNode.hs
@@ -4,8 +4,11 @@
 
 module Cooked.TestNode (nodeRun, emulatorRun) where
 
+import Cardano.Api qualified as Cardano
 import Control.Monad
 import Cooked
+import Data.Map qualified as Map
+import Optics.Core
 import Plutus.Script.Utils.Value qualified as Script
 import Polysemy
 
@@ -16,20 +19,39 @@ carrie = wallet 3
 david = wallet 4
 
 initDist :: InitialDistribution
-initDist = (alice `receives`) . Value . Script.ada <$> [30, 100]
+initDist = receives alice . Value . Script.ada <$> [30, 100, 40, 28]
 
-blockChainRun :: (Members DirectBlockChainEffs effs) => Sem effs ()
-blockChainRun = do
-  utxos <- utxosAt alice
+fromFaucetUtxo :: (Members DirectBlockChainEffs effs) => Sem effs ()
+fromFaucetUtxo = do
+  faucetUtxo <-
+    allUtxos
+      >>= ensureAFoldIs (txSkelOutOwnerL % userPubKeyHashAT)
+      >>= retrieve (fst . Map.elemAt 0)
   validateTxSkel_ $
     txSkelEmulatorTemplate
-      { txSkelInputs = emptyTxSkelRedeemer <$ utxos,
-        txSkelOutputs = [bob `receives` Value (Script.ada 40)],
+      { txSkelInputs = Map.singleton faucetUtxo emptyTxSkelRedeemer,
+        txSkelOutputs = initDist,
         txSkelSignatories = [signatoryWallet alice]
       }
 
+blockChainRun :: (Members DirectBlockChainEffs effs) => Sem effs ()
+blockChainRun =
+  validateTxSkel_ $
+    txSkelEmulatorTemplate
+      { txSkelOutputs = [bob `receives` Value (Script.ada 40)],
+        txSkelSignatories = [signatoryWallet alice]
+      }
+
+nodeConfig :: Cardano.LocalNodeConnectInfo
+nodeConfig =
+  Cardano.LocalNodeConnectInfo
+    { Cardano.localConsensusModeParams = Cardano.CardanoModeParams (Cardano.EpochSlots 86_400),
+      Cardano.localNodeNetworkId = Cardano.Testnet (Cardano.NetworkMagic undefined),
+      Cardano.localNodeSocketPath = Cardano.File undefined
+    }
+
 nodeRun :: IO ()
-nodeRun = runBlockChainFromConfTemplate @DirectBlockChainEffs undefined blockChainRun
+nodeRun = runBlockChainFromConfTemplate @DirectBlockChainEffs nodeConfig $ fromFaucetUtxo >> blockChainRun
 
 emulatorRun :: [MockChainReturn ()]
 emulatorRun = runMockChainFromInitDist @StagedMockChainEffs initDist $ do
