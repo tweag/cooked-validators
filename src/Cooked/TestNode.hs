ac05c955c3d5645a31faa9dfdec752362481d9ff
mmontin <o.montin@gmail.com>
2026-09-04 18:09:19 +0200

    update test

--- src/Cooked/TestNode.hs
+++ src/Cooked/TestNode.hs
@@ -2,12 +2,28 @@
 -- rename effect stacks to include MockChain
 -- signatory first input from pk ?
 
-module Cooked.TestNode (nodeRun, emulatorRun, exportSignatoryFiles) where
+module Cooked.TestNode
+  ( exportSignatoryFiles,
+    exportAlice,
+    exportBob,
+    fetchFrom,
+    alice,
+    bob,
+    carrie,
+    david,
+    fetchAll,
+    defineAll,
+    exportAddressFile,
+    runInIO,
+    pays,
+    nodeRun,
+  )
+where
 
 import Cardano.Api as Cardano
 import Cardano.Crypto.Wallet qualified as Crypto
-import Control.Monad
-import Cooked
+import Cooked hiding (Member)
+import Cooked.Utilities.UtxoSearch
 import Data.Map qualified as Map
 import Data.Text.IO qualified as T
 import Plutus.Script.Utils.Value qualified as Script
@@ -16,6 +32,12 @@ import Polysemy
 --------------------------------------------------------------------------------
 -- Export Wallets
 --------------------------------------------------------------------------------
+
+alice, bob, carrie, david :: Wallet
+alice = wallet 1
+bob = wallet 2
+carrie = wallet 3
+david = wallet 4
 
 exportSignatoryFiles :: NetworkId -> FilePath -> Crypto.XPrv -> IO ()
 exportSignatoryFiles netId filePrefix xprv = do
@@ -46,76 +68,75 @@ exportSignatoryFiles netId filePrefix xprv = do
 
       putStrLn $ "Successfully generated:\n  - " ++ skeyFile ++ "\n  - " ++ vkeyFile ++ "\n  - " ++ addrFile
 
+exportAddressFile :: String -> Wallet -> IO ()
+exportAddressFile name =
+  exportSignatoryFiles
+    (Testnet (NetworkMagic 42))
+    ("/home/monsieuro/tweag/cardano-peras/testnet/wallets/" <> name)
+    . walletSK
+
 exportAlice :: IO ()
-exportAlice = exportSignatoryFiles (Testnet (NetworkMagic 42)) "/Users/adithya/Desktop/Prog/cardano-peras/testnet/wallets/alice" (walletSK alice)
+exportAlice = exportAddressFile "alice" alice
+
+exportBob :: IO ()
+exportBob = exportAddressFile "bob" bob
 
 --------------------------------------------------------------------------------
 -- Main
 --------------------------------------------------------------------------------
-
-alice, bob, carrie, david :: Wallet
-alice = wallet 1
-bob = wallet 2
-carrie = wallet 3
-david = wallet 4
-
-initDist :: InitialDistribution
-initDist = receives alice . Value . Script.ada <$> [30, 100, 40, 28]
-
-aliceToAlice :: (Members DirectBlockChainEffs effs) => Sem effs ()
-aliceToAlice =
-  -- do
-  -- utxos <- utxosAt alice
-  validateTxSkel_ $
-    txSkelEmulatorTemplate
-      { -- txSkelInputs = emptyTxSkelRedeemer <$ utxos,
-        txSkelOutputs = initDist,
-        txSkelSignatories = [signatoryWallet alice]
-      }
-
-blockChainRun :: (Members DirectBlockChainEffs effs) => Sem effs ()
-blockChainRun =
-  validateTxSkel_ $
-    txSkelEmulatorTemplate
-      { txSkelOutputs = [bob `receives` Value (Script.ada 40)],
-        txSkelSignatories = [signatoryWallet alice]
-      }
 
 nodeConfig :: Cardano.LocalNodeConnectInfo
 nodeConfig =
   Cardano.LocalNodeConnectInfo
     { Cardano.localConsensusModeParams = Cardano.CardanoModeParams (Cardano.EpochSlots 86_400),
       Cardano.localNodeNetworkId = Cardano.Testnet (Cardano.NetworkMagic 42),
-      Cardano.localNodeSocketPath = Cardano.File "/Users/adithya/Desktop/Prog/cardano-peras/testnet/devnet-env/socket/node2/sock"
+      Cardano.localNodeSocketPath = Cardano.File "/home/monsieuro/tweag/cardano-peras/testnet/devnet-env/socket/node2/sock"
     }
 
+defineAll :: (Member Misc effs) => Sem effs ()
+defineAll = do
+  define_ "alice" alice
+  define_ "bob" bob
+  define_ "carrie" carrie
+  define_ "david" david
+
+fetchFrom :: (Members '[Misc, Query] effs) => Wallet -> String -> Sem effs ()
+fetchFrom wal title =
+  utxosAt wal
+    >>= retrieveUtxos
+    >>= retrieve Map.toList
+    >>= retrieve (fmap snd)
+    >>= noteL title
+
+fetchAll :: (Members '[Misc, Query] effs) => Sem effs ()
+fetchAll =
+  allUtxos
+    >>= retrieveUtxos
+    >>= retrieve Map.toList
+    >>= retrieve (fmap snd)
+    >>= noteL "All Utxos"
+
+runInIO :: (Show a) => DirectBlockChain a -> IO ()
+runInIO = runBlockChainFromConfTemplate nodeConfig
+
+pays :: Wallet -> Wallet -> Integer -> DirectBlockChain ()
+pays from to ada =
+  validateTxSkel_ $
+    txSkelNodeTemplate
+      { txSkelSignatories = txSkelSignatoriesFromList [from],
+        txSkelOutputs = [to `receives` Value (Script.ada ada)]
+      }
+
 nodeRun :: IO ()
-nodeRun = do
-  runBlockChainFromConfTemplate @DirectBlockChainEffs nodeConfig $ do
-    alice <- define "Alice" $ wallet 1
-    bob <- define "Bob" $ wallet 2
-
-    noteS "Start: Alice"
-    aliceToAlice
-    -- It waits here??
-    -- Is the scheduling off?
-
-    noteS "End: Alice"
-    noteS "Start: Bob"
-    blockChainRun
-    noteS "End: Bob"
-    bobUtxos <- utxosAt bob
-    noteS "Start: Wait"
-    -- Wait bug
-    waitNSlots 5
-    noteS "End: Wait"
-    -- waitTillExists
-    noteW (length bobUtxos)
-    -- prettyPrintBug
-    assertW "Bob has 5 utxos" $ length bobUtxos == 5
-
-emulatorRun :: [MockChainReturn ()]
-emulatorRun = runMockChainFromInitDist @StagedMockChainEffs initDist $ do
-  withTweak blockChainRun $
-    msum $
-      insertFirstTweak txSkelOutputsL . (`receives` Value (Script.ada 20)) <$> [carrie, david]
+nodeRun = runInIO $ do
+  defineAll
+  noteS "Alice pays to Bob"
+  pays alice bob 10
+  noteS "Waiting 60 slots"
+  _ <- waitNSlots 60
+  fetchFrom bob "Bob's Utxos"
+  noteS "Bob pays to David"
+  pays bob david 5
+  noteS "Waiting 60 slots"
+  _ <- waitNSlots 60
+  fetchFrom david "David's Utxos"
